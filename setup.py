9d07e8ec90d1e1eb8586c944dd77663f35c26b16
Romeo Orsolino <romeo.orsolino@iit.it>
2018-04-23 10:54:23 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 Romeo Orsolino <romeo.orsolino@iit.it>
+#
+# This file is part of legsthrust.
+#
+# legsthrust is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# legsthrust is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with legsthrust. If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+setup(
+    name='legsthrust',
+    version='0.1.0',
+    description="Python Polyhedron Manipulation",
+    url="https://github.com/stephane-caron/legsthrust",
+    author="Stéphane Caron",
+    author_email="stephane.caron@lirmm.fr",
+    license="LGPL",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License (LGPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Mathematics'],
+    packages=['legsthrust']
+)
