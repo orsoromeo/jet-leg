f81c759a2c420dd4302215d5ed488beec0a0ca19
Romeo Orsolino <romeo.orsolino@iit.it>
2018-05-26 18:53:17 +0200

    plotting the results of the stability check

--- legsthrust/check_stability_LP.py
+++ legsthrust/check_stability_LP.py
@@ -7,6 +7,9 @@ Created on Sat May 26 15:09:24 2018
 import cvxopt
 from cvxopt import matrix, solvers
 import numpy as np
+# for plotting
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def skew(v):
     if len(v) == 4: v = v[:3]/v[3]
@@ -63,23 +66,50 @@ print G, h
 print np.size(G,0), np.size(G,1)
 
 # Equality constraints
-com = np.array([0.0, 2.0, 0.0])
+com = np.array([-20.0, -10.0, 0.0])
 print com, grav
-torque = np.cross(com, np.transpose(grav))
+torque = -np.cross(com, np.transpose(grav))
+print 'torque: ', torque
 A = np.zeros((6,0))
 for j in range(0,nc):
     r = contacts[j,:]
     GraspMat = getGraspMatrix(r)
     A = np.hstack((A, GraspMat[:,0:3]))
 A = matrix(A)
-b = matrix(np.vstack([-grav, np.zeros((3,1))]).reshape((6)))
+b = matrix(np.vstack([-grav, np.transpose(torque)]).reshape((6)))
 #A = matrix([1.0, 1.0], (1,2))
 #b = matrix(1.0)
 print A, b
 print np.size(A,0), np.size(A,1)
 
 sol=solvers.lp(p, G, h, A, b)
-print(sol['x'])
+x = sol['x']
+print sol
+status = sol['status']
+print status
+print x
 print(sol['primal objective'])
+
+feasible_points = np.zeros((0,3))
+unfeasible_points = np.zeros((0,3))
+print len(com), len(feasible_points)
+if status == 'optimal':
+    feasible_points = np.vstack([feasible_points,com])
+else:
+    unfeasible_points = np.vstack([unfeasible_points,com])
+   
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(contacts[:,0], contacts[:,1], contacts[:,2],c='b',s=70)
+if np.size(feasible_points,0) != 0:
+    ax.scatter(feasible_points[:,0], feasible_points[:,1], feasible_points[:,2],c='g',s=50)
+if np.size(unfeasible_points,0) != 0:
+    ax.scatter(unfeasible_points[:,0], unfeasible_points[:,1], unfeasible_points[:,2],c='r',s=50)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
 
 print "bye bye" 
