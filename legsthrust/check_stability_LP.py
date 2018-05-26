2e36f7b1c876179341270439880d8229fe8db7a3
Romeo Orsolino <romeo.orsolino@iit.it>
2018-05-26 20:33:22 +0200

    checking both friction and actuation constraint together

--- legsthrust/check_stability_LP.py
+++ legsthrust/check_stability_LP.py
@@ -40,7 +40,17 @@ def linear_cone_constraint(n, mu):
     u = np.dot(n,mu)
     #cone_constraints = m - np.transpose(u)
     cone_constraints = np.vstack((m - np.transpose(u), - m - np.transpose(u)))
-    return cone_constraints
+    known_term = np.zeros((6,1))
+    return cone_constraints, known_term
+    
+def actuation_polygon_constraint():
+    dx = 100
+    dy = 100
+    dz = 700
+    constraint = np.vstack([np.eye(3), -np.eye(3)])
+    print constraint
+    known_term = np.array([[dx],[dy],[dz],[dx],[dy],[dz]])
+    return constraint, known_term
     
 def normalize(n):
     norm1 = np.linalg.norm(n)
@@ -53,9 +63,10 @@ mass = 100
 grav = np.array([[0.], [0.], [-g*mass]])
 # contact points
 r1 = np.array([0.0, 10.0, 2.0])
-r2 = np.array([10.0, -10.0, -2.0])
+r2 = np.array([10.0, -10.0, 1.5])
 r3 = np.array([-10.0, -10.0, 1.0])
-contacts = np.vstack((r1, r2, r3))
+r4 = np.array([0.0, -20.0, 1.0])
+contacts = np.vstack((r1, r2, r3, r4))
 # contact surface normals
 n1 = np.array([[0.0], [0.0], [1.0]])
 n1 = normalize(n1)
@@ -63,27 +74,42 @@ n2 = np.array([[0.0], [0.0], [1.0]])
 n2 = normalize(n2)
 n3 = np.array([[0.0], [0.0], [1.0]])
 n3 = normalize(n3)
-normals = np.hstack((n1, n2, n3))
+n4 = np.array([[0.0], [0.0], [1.0]])
+n4 = normalize(n4)
+
+normals = np.hstack((n1, n2, n3, n4))
+
 
 friction_coeff = 1.0
 
-cons = np.zeros((0,0))
-for j in range(0,nc):
-    c = linear_cone_constraint(normals[:,j],friction_coeff)
-    cons = np.block([[cons, np.zeros((np.size(cons,0),np.size(c,1)))],
-                  [np.zeros((np.size(c,0),np.size(cons,1))), c]])
 #print C
 Q = 2*matrix(np.zeros((3*nc,3*nc)))
 p = matrix(np.ones((3*nc,1)))
 print Q, p
 print np.size(Q,0), np.size(Q,1)
 
+cons1 = np.zeros((0,0))
+h_vec1 = np.zeros((0,1))
+cons2 = np.zeros((0,0))
+h_vec2 = np.zeros((0,1))
+for j in range(0,nc):
+    c, h_term = linear_cone_constraint(normals[:,j],friction_coeff)
+    cons1 = np.block([[cons1, np.zeros((np.size(cons1,0),np.size(c,1)))],
+                  [np.zeros((np.size(c,0),np.size(cons1,1))), c]])
+    h_vec1 = np.vstack([h_vec1, h_term])
+    c, h_term = actuation_polygon_constraint()
+    cons2 = np.block([[cons2, np.zeros((np.size(cons2,0),np.size(c,1)))],
+                  [np.zeros((np.size(c,0),np.size(cons2,1))), c]])
+    h_vec2 = np.vstack([h_vec2, h_term])
+
+cons = np.vstack([cons1, cons2])
+h_vec = np.vstack([h_vec1, h_vec2])
 # Inequality constraints
-m_ineq = 6*nc
+m_ineq = np.size(cons,0)
 #A=A.astype(double) 
 #cons = cons.astype(np.double)
 G = matrix(cons) #matrix([[-1.0,0.0],[0.0,-1.0]])
-h = matrix(np.zeros((m_ineq,1)).reshape(m_ineq)) #matrix([0.0,0.0])
+h = matrix(h_vec.reshape(m_ineq)) #matrix([0.0,0.0])
 print G, h
 print np.size(G,0), np.size(G,1)
 
@@ -91,7 +117,7 @@ feasible_points = np.zeros((0,3))
 unfeasible_points = np.zeros((0,3))
 # Equality constraints
 for com_x in np.arange(-25,25,1.):
-    for com_y in np.arange(-30,15,1.):
+    for com_y in np.arange(-20,15,1.):
         com = np.array([com_x, com_y, 0.0])
         torque = -np.cross(com, np.transpose(grav))
         A = np.zeros((6,0))
@@ -117,8 +143,8 @@ for com_x in np.arange(-25,25,1.):
 # Plotting the results   
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(contacts[:,0], contacts[:,1], contacts[:,2],c='b',s=70)
 for j in range(0,nc):
+    ax.scatter(contacts[j,0], contacts[j,1], contacts[j,2],c='b',s=100)
     a = Arrow3D([contacts[j,0], contacts[j,0]+normals[0,j]], [contacts[j,1], contacts[j,1]+normals[1,j]],[contacts[j,2], contacts[j,2]+normals[2,j]], mutation_scale=20, lw=3, arrowstyle="-|>", color="r")
     ax.add_artist(a)
     
