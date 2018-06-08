9b9570fd5e77f64171b195d5b1fe8e594518dcae
Michele Focchi <michele.focchi@iit.it>
2018-06-08 16:50:54 +0200

    speed of computation improved on 1 order of magnitude!

--- legsthrust/anaylitic_test_three_contacts.py
+++ legsthrust/anaylitic_test_three_contacts.py
@@ -19,12 +19,29 @@ def minksum(a,b):
     n_a = np.size(a,1)
     n_b = np.size(b,1)
     #n_sum = n_a*n_b
-    sum = np.zeros((1,np.size(a,0)))
+    sum = np.zeros((0,np.size(a,0)))
     for j in range(0,n_a):
         for i in range(0,n_b):
             sum = np.vstack([sum, a[:,j] + b[:,i]])
         
-    return np.transpose(sum)    
+
+        
+    return np.transpose(sum)   
+
+def convex_hull(input_matrix):#requires 6XN
+    
+    hull_matrix = np.zeros((0,6))  
+    input_matrix_t = np.transpose(input_matrix)
+    hull = scipy.spatial.ConvexHull(input_matrix_t, qhull_options="Qx") #is pX6    
+    #get the matrix
+    indices = hull.vertices
+    n = len(indices)    
+    #print n
+    for i in range(n):
+        hull_matrix = np.vstack([hull_matrix, input_matrix_t[indices[i]]])   
+    
+    return np.transpose(hull_matrix)
+
 
 def compute_points(a, mg):
     n_a = np.size(a,1)
@@ -39,7 +56,7 @@ def compute_points(a, mg):
                 if(alpha>=0.0)&(alpha<=1.0):
                     new_point = lambda1 + (lambda2 - lambda1)*alpha
                     points = np.vstack([points, new_point])
-    print np.size(points,0)
+    print 'number of edges', np.size(points,0)
     return np.transpose(points), np.size(points,0)
     
 
@@ -54,22 +71,24 @@ def project_points(vertices, mass):
         v_new_y = tau_x / mass
         v_new = np.array([[v_new_x],[v_new_y]])
         vertices2d = np.hstack([vertices2d, v_new])
-    print np.size(vertices2d,1)
+    print 'number of com points', np.size(vertices2d,1)
     return vertices2d
     
+'''MAIN'''
 r1 = np.array([1.0, 0.0, 0.0])
 r2 = np.array([2.3, 1.5, 0.0])
 r3 = np.array([3.0, 0.2, 0.0])
 contacts = np.vstack([r1, r2, r3, r1])
-dx = 100
-dy = 100
-dz = 100
-vertices = np.array([[dx, dx, -dx, -dx, dx, dx, -dx, -dx],
-                     [dy, -dy, -dy, dy, dy, -dy, -dy, dy],
-                     [dz, dz, dz, dz, -dz, -dz, -dz, -dz]])
-#vertices = np.array([[0., dx, dx, -dx, -dx],
-#                         [0., dy, -dy, -dy, dy],
-#                         [0., dz, dz, dz, dz]])
+mg = 170
+dx = 100*mg
+dy = 100*mg
+dz = 100*mg
+#vertices = np.array([[dx, dx, -dx, -dx, dx, dx, -dx, -dx],
+#                     [dy, -dy, -dy, dy, dy, -dy, -dy, dy],
+#                     [dz, dz, dz, dz, -dz, -dz, -dz, -dz]])
+vertices = np.array([[0., dx, dx, -dx, -dx],
+                         [0., dy, -dy, -dy, dy],
+                         [0., dz, dz, dz, dz]])
 tau1 = np.zeros((3,np.size(vertices,1)))
 tau2 = np.zeros((3,np.size(vertices,1)))
 tau3 = np.zeros((3,np.size(vertices,1)))             
@@ -79,37 +98,23 @@ for j in range(0,np.size(vertices,1)):
     tau3[:,j] = np.cross(r3, vertices[:,j])
 w1 = np.vstack([tau1, vertices])
 w2 = np.vstack([tau2, vertices])
-w3 = np.vstack([tau3, vertices])
+w3 = np.vstack([tau3, vertices]) #6XN
 print w1, w2, w3
 w12 = minksum(w1, w2)
 w123 = minksum(w12, w3) #CWC con punti interni 6XN
-w123_t = np.transpose(w123);  #CWC con punti interni NX6
-
-chull_w123 = scipy.spatial.ConvexHull(w123_t, qhull_options="Qx")
-#get the matrix
-#w123_hull = w123_t[chull_w123]
-indices = chull_w123.vertices
-n = np.size(indices,0)
-w123_hull = np.zeros((0,6))
-
-for i in range(len(indices)):
-		  w123_hull = np.vstack([w123_hull, w123_t[indices[i]]])
 
 
+print 'number of vertex before convex hull', np.size(w123,1)
+w123_hull = convex_hull(w123)
+print 'number of vertex after convex hull', np.size(w123_hull,1)
 
-#fill in a new matrix
-#print w123_hull
+points, points_num = compute_points(w123_hull, mg) #compute edges and slice them with mg
+points2d = project_points(points, mg) #cimpute com points
 
-#print w123, np.size(w123,1)
-
-mass = 170
-points, points_num = compute_points(np.transpose(w123_hull), mass)
-points2d = project_points(points, mass)
-print np.size(points2d,0)
 #print points
 vertices2d = np.transpose(points2d)
 chull = scipy.spatial.ConvexHull(vertices2d)
-print chull.vertices[0]
+#print chull.vertices[0]
 print("--- %s seconds ---" % (time.time() - start_time))
 
 plt.plot(contacts[:,0],contacts[:,1],'--b')
