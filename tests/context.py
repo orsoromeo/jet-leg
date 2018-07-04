fca5048b395a0295cd30571289b1cb8031b26dbe
Romeo Orsolino <rorsolino@ihmc.us>
2018-07-03 20:07:36 -0500

    added context.py for building package

---
+++ tests/context.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  3 19:52:23 2018
+
+@author: romeoorsolino
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import legsthrust
