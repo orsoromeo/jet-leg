afc25439ace55222cf3faca256cf36a7ccfcb4bf
Romeo Orsolino <rorsolino@ihmc.us>
2018-07-03 20:32:34 -0500

    added automatic unit tests discovery

--- tests/test_hyq_kinematics.py
+++ tests/test_hyq_kinematics.py
@@ -14,7 +14,10 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
     
-    def test_kinematics(self):
+    epsilon = 10e-03
+    assertPrecision = 3
+    
+    def test_inverse_kinematics(self):
         
         hyqKin = HyQKinematics()
         
@@ -43,7 +46,8 @@ class TestStringMethods(unittest.TestCase):
         #print new_contacts
         
         error = np.subtract(contacts,new_contacts)
-        print "feet error: ", np.sqrt(np.sum(error*error))
+        feet_pos_error =  np.sqrt(np.sum(error*error))
+        #print "feet error: ", np.sqrt(np.sum(error*error))
         
         new_q, q_dot, J_LF, J_RF, J_LH, J_RH = hyqKin.inverse_kin(np.transpose(new_contacts[:,0]),
                                                                   np.transpose(foot_vel[:,0]),
@@ -52,8 +56,40 @@ class TestStringMethods(unittest.TestCase):
                                                         
         #print new_q
         joints_error = np.sqrt(np.sum((q-new_q)*(q-new_q)))
-        print "joints error: ", joints_error
-        self.assertTrue( np.abs(joints_error-0.43) < 0.1)
+        #print "joints error: ", joints_error
+        self.assertTrue( np.abs(joints_error) < self.epsilon)
         
-if __name__ == '__main__':
-    unittest.main()
+        
+    def test_forward_kinematics(self):
+        
+        hyqKin = HyQKinematics()
+        
+        hyqKin.init_jacobians()
+        hyqKin.init_homogeneous()
+        
+        foot_vel = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]])
+        
+        LF_foot = np.array([0.3, 0.2, -0.58])
+        RF_foot = np.array([0.3, -0.2, -0.58])
+        LH_foot = np.array([-0.3, 0.2, -0.58])
+        RH_foot = np.array([-0.3, -0.2, -0.58])
+        contacts = np.vstack((LF_foot,RF_foot,LH_foot,RH_foot))
+        
+        #print contacts
+        
+        q, q_dot, J_LF, J_RF, J_LH, J_RH = hyqKin.inverse_kin(np.transpose(contacts[:,0]),
+                                                              np.transpose(foot_vel[:,0]),
+                                                        np.transpose(contacts[:,2]),
+                                                        np.transpose(foot_vel[:,2]))
+                                                        
+        #print q
+        hyqKin.update_homogeneous(q)
+        hyqKin.update_jacobians(q)
+        new_contacts = hyqKin.forward_kin(q)
+        #print new_contacts
+        
+        error = np.subtract(contacts,new_contacts)
+        feet_pos_error =  np.sqrt(np.sum(error*error))
+        #print "feet error: ", np.sqrt(np.sum(error*error))
+
+        self.assertAlmostEqual(feet_pos_error, 0.0, places = self.assertPrecision)
---
+++ tests/unit_test_main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  3 20:20:38 2018
+
+@author: Romeo Orsolino
+"""
+
+import unittest
+loader = unittest.TestLoader()
+tests = loader.discover('.')
+testRunner = unittest.runner.TextTestRunner()
+testRunner.run(tests)
