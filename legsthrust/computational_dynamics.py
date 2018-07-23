e4fbb8302695bd522b815b468f78d86ca1fc9533
Romeo Orsolino <rorsolino@ihmc.us>
2018-07-22 23:42:32 -0500

    created setup_lp function in computational dynamics

--- legsthrust/computational_dynamics.py
+++ legsthrust/computational_dynamics.py
@@ -129,75 +129,85 @@ class ComputationalDynamics():
 
     def LP_projection(self, constraint_mode, contacts, normals, mass, friction_coeff, ng, nc, mu, useVariableJacobian = False, stepX = 0.05, stepY = 0.05, stepZ = 0.05):
         start_t_LP = time.time()
-        g = 9.81    
-        grav = np.array([[0.], [0.], [-g*mass]])
-        p = matrix(np.zeros((3*nc,1)))    
-        
-        #kin = Kinematics()
-        kin = HyQKinematics()
-        constraint = Constraints()                              
-                
-        foot_vel = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]])
-        contactsFourLegs = np.vstack([contacts, np.zeros((4-nc,3))])
-        q, q_dot, J_LF, J_RF, J_LH, J_RH, isOutOfWorkspace = kin.inverse_kin(np.transpose(contactsFourLegs[:,0]),
-                                                  np.transpose(foot_vel[:,0]),
-                                                    np.transpose(contactsFourLegs[:,1]),
-                                                    np.transpose(foot_vel[:,1]),
-                                                    np.transpose(contactsFourLegs[:,2]),
-                                                    np.transpose(foot_vel[:,2]))
-        J_LF, J_RF, J_LH, J_RH = kin.update_jacobians(q)
-        G, h, isConstraintOk = constraint.inequalities(constraint_mode, nc, ng, normals, friction_coeff, J_LF, J_RF, J_LH, J_RH)
-        #print G, h
-        #print np.size(G,0), np.size(G,1)
-        
         feasible_points = np.zeros((0,3))
         unfeasible_points = np.zeros((0,3))
-        contact_forces = np.zeros((0,nc*3))
-        
+        contact_forces = np.zeros((0,nc*3))  
+        verbose = True
         """ Defining the equality constraints """
         for com_x in np.arange(-0.5,0.5,stepX):
             for com_y in np.arange(-0.4,0.4,stepY):
                 for com_z in np.arange(-0.2,0.25,stepZ):
                     com = np.array([com_x, com_y, com_z])
-                    torque = -np.cross(com, np.transpose(grav))
-                    A = np.zeros((6,0))
-                    for j in range(0,nc):
-                        r = contacts[j,:]
-                        GraspMat = self.getGraspMatrix(r)
-                        A = np.hstack((A, GraspMat[:,0:3]))
-                        A = matrix(A)
-                        b = matrix(np.vstack([-grav, np.transpose(torque)]).reshape((6)))
                         
-                    #contactsFourLegs = np.vstack([contacts, np.zeros((4-nc,3))])\
-                    if (useVariableJacobian):
-                        #contacts_new_x = contactsFourLegs[:,0] + com_x
-                        q, q_dot, J_LF, J_RF, J_LH, J_RH, isOutOfWorkspace = kin.inverse_kin(np.transpose(contactsFourLegs[:,0] - com_x),
-                                                  np.transpose(foot_vel[:,0]),
+                    p, G, h, A, b, isConstraintOk = self.setup_lp(mass, contacts, nc, ng, normals, com, constraint_mode, friction_coeff, useVariableJacobian)                 
+
+                    if not isConstraintOk:
+                        if verbose:
+                            print 'something is wrong in the inequalities'
+                    else:
+                        
+                        sol=solvers.lp(p, G, h, A, b)
+                        x = sol['x']
+                        status = sol['status']
+                        #print x
+                        if status == 'optimal':
+                            feasible_points = np.vstack([feasible_points,com])
+                            contact_forces = np.vstack([contact_forces, np.transpose(x)])
+                        else:
+                            unfeasible_points = np.vstack([unfeasible_points,com])
+                    
+        print("LP test: --- %s seconds ---" % (time.time() - start_t_LP))
+        
+        return feasible_points, unfeasible_points, contact_forces
+
+    def setup_lp(self, mass, contacts, nc, numberOfGenerators, normals, comWorldFrame, constraint_mode, friction_coeff, useVariableJacobian):
+        g = 9.81    
+        grav = np.array([[0.], [0.], [-g*mass]])
+        com_x = comWorldFrame[0]
+        com_y = comWorldFrame[1]
+        com_z = comWorldFrame[2]
+        p = matrix(np.zeros((3*nc,1)))                        
+        #kin = Kinematics()
+        kin = HyQKinematics()
+        constraint = Constraints()                              
+        
+        foot_vel = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]])
+        contactsFourLegs = np.vstack([contacts, np.zeros((4-nc,3))])
+        kin.inverse_kin(np.transpose(contactsFourLegs[:,0]),
+                        np.transpose(foot_vel[:,0]),
+                        np.transpose(contactsFourLegs[:,1]),
+                        np.transpose(foot_vel[:,1]),
+                        np.transpose(contactsFourLegs[:,2]),
+                        np.transpose(foot_vel[:,2]))
+        
+        torque = -np.cross(comWorldFrame, np.transpose(grav)) 
+        A = np.zeros((6,0))
+        for j in range(0,nc):
+            r = contacts[j,:]
+            GraspMat = self.getGraspMatrix(r)
+            A = np.hstack((A, GraspMat[:,0:3]))
+            A = matrix(A)
+            b = matrix(np.vstack([-grav, np.transpose(torque)]).reshape((6)))
+            
+        #contactsFourLegs = np.vstack([contacts, np.zeros((4-nc,3))])\
+        if (useVariableJacobian):
+            #contacts_new_x = contactsFourLegs[:,0] + com_x
+            q, q_dot, J_LF, J_RF, J_LH, J_RH, isOutOfWorkspace = kin.inverse_kin(np.transpose(contactsFourLegs[:,0] - com_x),
+                                              np.transpose(foot_vel[:,0]),
                                                     np.transpose(contactsFourLegs[:,1] - com_y),
                                                     np.transpose(foot_vel[:,1]),
                                                     np.transpose(contactsFourLegs[:,2] - com_z),
                                                     np.transpose(foot_vel[:,2]))
-                    if (not isOutOfWorkspace):
-                        #kin.update_jacobians(q)
-                        J_LF, J_RF, J_LH, J_RH = kin.update_jacobians(q)
-                        #print J_LF
-                        G, h, isConstraintOk = constraint.inequalities(constraint_mode, nc, ng, normals, friction_coeff, J_LF, J_RF, J_LH, J_RH)
-                        #print G, h
-                        if not isConstraintOk:
-                            print 'something is wrong in the inequalities'
-                        else:
-                            sol=solvers.lp(p, G, h, A, b)
-                            x = sol['x']
-                            status = sol['status']
-                            #print x
-                            if status == 'optimal':
-                                feasible_points = np.vstack([feasible_points,com])
-                                contact_forces = np.vstack([contact_forces, np.transpose(x)])
-                            else:
-                                unfeasible_points = np.vstack([unfeasible_points,com])
-                    
+        if (not isOutOfWorkspace):
+            #kin.update_jacobians(q)
+            J_LF, J_RF, J_LH, J_RH = kin.update_jacobians(q)
+            #print J_LF
+            G, h, isLpOK = constraint.inequalities(constraint_mode, nc, numberOfGenerators, normals, friction_coeff, J_LF, J_RF, J_LH, J_RH)
+            #print G, h
+        else:
+            isLpOK = False
+            G= [None]
+            h = [None]
         
-        
-        print("LP test: --- %s seconds ---" % (time.time() - start_t_LP))
-        
-        return feasible_points, unfeasible_points, contact_forces
+        lp = p, G, h, A, b
+        return p, G, h, A, b, isLpOK 
