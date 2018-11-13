fixed tests on path iterative projection along a given direction and actuation region computation

examples/actuation_region_example.py
@@ -30,8 +30,8 @@ pathIP = PathIterativeProjection()
 # number of contacts
 number_of_contacts = 3
 
-# ONLY_ACTUATION or ONLY_FRICTION
-constraint_mode = 'ONLY_ACTUATION'
+# ONLY_ACTUATION, ONLY_FRICTION or FRICTION_AND_ACTUATION
+constraint_mode = 'FRICTION_AND_ACTUATION'
 useVariableJacobian = True
 trunk_mass = 100
 mu = 0.8
@@ -45,21 +45,42 @@ iterProj = PathIterativeProjection()
 
 #print "direction: ", desired_direction
 """ contact points """
-LF_foot = np.array([0.4, 0.3, -0.5])
-RF_foot = np.array([0.4, -0.3, -0.5])
-terrainHeight = terrain.get_height(-.4, 0.3)
-LH_foot = np.array([-.4, 0.3, terrainHeight-0.5])     
+#LF_foot = np.array([0.4, 0.3, -0.5])
+#RF_foot = np.array([0.4, -0.3, -0.5])
+#terrainHeight = terrain.get_height(-.4, 0.3)
+#LH_foot = np.array([-.4, 0.3, terrainHeight-0.5])     
+#RH_foot = np.array([-0.3, -0.2, -0.5])
+
+LF_foot = np.array([0.3, 0.2, -0.5])
+RF_foot = np.array([0.3, -0.2, -0.5])
+LH_foot = np.array([-0.3, 0.2, -0.5])
 RH_foot = np.array([-0.3, -0.2, -0.5])
+contacts = np.vstack((LF_foot,RF_foot,LH_foot,RH_foot))
+stanceLegs = [1,1,0,1]
+stanceIndex = []
+swingIndex = []
+print 'stance', stanceLegs
+for iter in range(0, 4):
+    if stanceLegs[iter] == 1:
+#               print 'new poly', stanceIndex, iter
+        stanceIndex = np.hstack([stanceIndex, iter])
+    else:
+        swingIndex = iter
 
-contactsToStack = np.vstack((LF_foot,RF_foot,LH_foot,RH_foot))
-contacts = contactsToStack[0:number_of_contacts, :]
 
+LF_tau_lim = [50.0, 100.0, 100.0]
+RF_tau_lim = [50.0, 100.0, 100.0]
+LH_tau_lim = [50.0, 100.0, 100.0]
+RH_tau_lim = [50.0, 100.0, 100.0]
+torque_limits = np.array([LF_tau_lim, RF_tau_lim, LH_tau_lim, RH_tau_lim])
+comWF = np.array([0.0,0.0,0.0])
+    
 tolerance = 0.005
 
-for angle in np.arange(-np.pi, np.pi, 0.3):
+for angle in np.arange(-np.pi, np.pi, 0.5):
     print 'new search angle: ', angle
     desired_direction = np.array([np.cos(angle), np.sin(angle), 0.0])
-    newLimitPoint, stackedErrors = pathIP.find_vertex_along_path(constraint_mode, contacts, comWF, desired_direction, tolerance)
+    newLimitPoint, stackedErrors = pathIP.find_vertex_along_path(constraint_mode, contacts, comWF, desired_direction, stanceLegs, stanceIndex, swingIndex, torque_limits, tolerance)
     comTrajectoriesToStack = np.vstack([comTrajectoriesToStack, newLimitPoint])
 
 

examples/communication.py
@@ -97,7 +97,7 @@ class HyQSim(threading.Thread):
     def send_force_polygons(self, name, polygons):
 #        self.output = dict()
         output = LegsPolygons()
-        output.names = name
+#        output.names = name
         output.polygons = polygons
         self.pub_force_polygons.publish(output) 
         
@@ -262,13 +262,15 @@ def talker():
     p.register_node()
     name = "Actuation_region"
     point = Point()
-    actPolygon = LegsPolygons()
+    polygonVertex = Point()
+    actPolygon = Polygon3D()
     actuationParams = ActuationParameters()
     i = 0
 
     while not ros.is_shutdown():
         vertices1 = [point]
-        poly = []
+        actPolygons = [polygonVertex]
+#        poly = []
 #        print("Time: " + str(i*0.004) + "s and Simulation time: " + str(p.get_sim_time()/60))
         p.get_sim_wbs()
         actuationParams.getParams(p.hyq_rcf_debug)
@@ -296,25 +298,28 @@ def talker():
         
         point.x = IAR[0][0]
         point.y = IAR[0][1]
-        point.z =  -0.2
+        point.z =  -0.55
         vertices1 = [point]
-#        poly = 
+        
         for i in range(0, num_actuation_vertices):
             point = Point()
             point.x = IAR[i][0]
             point.y = IAR[i][1]
-            point.z = -0.2
+            point.z = -0.55
             vertices1 = np.hstack([vertices1, point])
 
+        poly = [actPolygon]
         for i in range(0, nc):
-            vx = Point()
+            actPolygons = [polygonVertex]            
             for j in range(0,7):
+                vx = Point()
 #                print 'act', actuation_polygons_array[i][0][j]
                 vx.x = actuation_polygons_array[i][0][j]
                 vx.y = actuation_polygons_array[i][1][j]
                 vx.z = actuation_polygons_array[i][2][j]
-                actPolygon = np.hstack([actPolygon, vx])          
-            poly = np.hstack([poly, actPolygon])  
+#                print vx
+                actPolygons = np.hstack([actPolygons, vx])          
+            poly = np.hstack([poly, actPolygons])  
             
         p.send_actuation_polygons(name, vertices1)
         
@@ -327,13 +332,13 @@ def talker():
         num_support_vertices = np.size(IAR, 0)
         point.x = IAR[0][0]
         point.y = IAR[0][1]
-        point.z =  -0.2
+        point.z =  -0.55
         vertices2 = [point]
         for i in range(0, num_support_vertices):
             point = Point()
             point.x = IAR[i][0]
             point.y = IAR[i][1]
-            point.z = -0.2
+            point.z = -0.55
             vertices2 = np.hstack([vertices2, point])
             
 #        print'vertices', vertices2        

examples/path_sequential_IP_example.py
@@ -27,8 +27,8 @@ pathIP = PathIterativeProjection()
 # number of contacts
 number_of_contacts = 3
 
-# ONLY_ACTUATION or ONLY_FRICTION
-constraint_mode = 'ONLY_ACTUATION'
+# ONLY_ACTUATION, ONLY_FRICTION or FRICTION_AND_ACTUATION
+constraint_mode = 'FRICTION_AND_ACTUATION'
 useVariableJacobian = True
 trunk_mass = 100
 mu = 0.8
@@ -45,13 +45,32 @@ terrainHeight = terrain.get_height(-.4, 0.3)
 LH_foot = np.array([-.4, 0.3, terrainHeight-0.5])     
 RH_foot = np.array([-0.3, -0.2, -0.5])
 
-angle = np.random.normal(3.1415, 0.2)
+angle = np.random.normal(np.pi, 0.2)
 desired_direction = np.array([np.cos(angle), np.sin(angle), 0.0])
-contactsToStack = np.vstack((LF_foot,RF_foot,LH_foot,RH_foot))
-contacts = contactsToStack[0:number_of_contacts, :]
+contacts = np.vstack((LF_foot,RF_foot,LH_foot,RH_foot))
+
+stanceLegs = [1,1,1,1]
+stanceIndex = []
+swingIndex = []
+print 'stance', stanceLegs
+for iter in range(0, 4):
+    if stanceLegs[iter] == 1:
+#               print 'new poly', stanceIndex, iter
+        stanceIndex = np.hstack([stanceIndex, iter])
+    else:
+        swingIndex = iter
+
+print stanceIndex, swingIndex
+
+LF_tau_lim = [50.0, 100.0, 100.0]
+RF_tau_lim = [50.0, 100.0, 100.0]
+LH_tau_lim = [50.0, 100.0, 100.0]
+RH_tau_lim = [50.0, 100.0, 100.0]
+torque_limits = np.array([LF_tau_lim, RF_tau_lim, LH_tau_lim, RH_tau_lim])
+comWF = np.array([0.0,0.0,0.0])
 
 tolerance = 0.005
-newLimitPoint, stackedErrors = pathIP.find_vertex_along_path(constraint_mode, contacts, comWF, desired_direction, tolerance)
+newLimitPoint, stackedErrors = pathIP.find_vertex_along_path(constraint_mode, contacts, comWF, desired_direction, stanceLegs, stanceIndex, swingIndex, torque_limits, tolerance)
 
 print 'Errors convergence: ', stackedErrors
 

jet_leg/computational_dynamics.py
@@ -109,24 +109,31 @@ class ComputationalDynamics():
         #C_force = constr.linearized_cone_halfspaces(ng, mu)
         # Inequality matrix for stacked contact forces in world frame:
         if constraint_mode == 'ONLY_FRICTION':
-            print contactsNumber
+#            print contactsNumber
             C, d = self.constr.linearized_cone_halfspaces_world(contactsNumber, ng, mu, normals)
 #            print np.size(C,0), np.size(C,1), C
 #            print C,d
         if constraint_mode == 'FRICTION_AND_ACTUATION':
             C1, d1, actuation_polygons = self.constr.compute_actuation_constraints(contacts, comWF, stanceLegs, stanceIndex, swingIndex, torque_limits, trunk_mass)                           
             C2, d2 = self.constr.linearized_cone_halfspaces_world(contactsNumber, ng, mu, normals)
-                
-            C = np.vstack([C1, C2])
+#            print C1, C2
+            if d1.shape[0] is not 1:
+                print d1
+                C = np.vstack([C1, C2])
 #               print np.size(C,0), np.size(C,1), C
-            d = np.hstack([d1[0], d2])
+                d = np.hstack([d1[0], d2])
 #                print d
-            d = d.reshape((6+ng)*contactsNumber)
+                d = d.reshape((6+ng)*contactsNumber)
+            else:
+                C = C2
+                d = d2
             
         if constraint_mode == 'ONLY_ACTUATION':
             C, d, actuation_polygons = self.constr.compute_actuation_constraints(contacts, comWF, stanceLegs, stanceIndex, swingIndex, torque_limits, trunk_mass)
-                  
-            d = d.reshape(6*contactsNumber)        
+#            print d.shape[0]            
+            if d.shape[0] is not 1:
+#                print d
+                d = d.reshape(6*contactsNumber)        
         
         ineq = (C, d)  # C * x <= d
                 

jet_leg/constraints.py
@@ -17,8 +17,8 @@ class Constraints:
     
     def compute_actuation_constraints(self, contactsWF, comWF, stanceLegs, stanceIndex, swingIndex, torque_limits, trunk_mass):
         foot_vel = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]])
-#            print '4 legs', contactsFourLegs
         contactsBF = contactsWF - comWF
+#        print contactsBF
         q, q_dot, J_LF, J_RF, J_LH, J_RH, isOutOfWorkspace = self.kin.inverse_kin(np.transpose(contactsBF[:,0]),
                                                   np.transpose(foot_vel[:,0]),
                                                     np.transpose(contactsBF[:,1]),
@@ -28,8 +28,10 @@ class Constraints:
         J_LF, J_RF, J_LH, J_RH = self.kin.update_jacobians(q)
 
         if isOutOfWorkspace:
-            C = np.zeros((0,0))
-            d = np.zeros((1,0))
+            C1 = np.zeros((0,0))
+            d1 = np.zeros((1,0))
+            actuation_polygons = []
+            print 'Out of workspace IK!!!'
         else:
             jacobianMatrices = np.array([J_LF, J_RF, J_LH, J_RH])
             actuation_polygons = self.computeActuationPolygons(stanceLegs, stanceIndex, swingIndex, jacobianMatrices, torque_limits)
@@ -42,10 +44,11 @@ class Constraints:
             contactsNumber = np.sum(stanceLegs)
 #                actuation_polygons = np.array([act_LF,act_RF,act_LH,act_RH])
             for j in range (0,contactsNumber):
-                print 'second iter', j, actuation_polygons[j]
+#                print 'second iter', j, actuation_polygons[j]
                 hexahedronHalfSpaceConstraints, knownTerm = self.hexahedron(actuation_polygons[j]/trunk_mass)
                 C1 = block_diag(C1, hexahedronHalfSpaceConstraints)
-                d1 = np.hstack([d1, knownTerm.T])        
+                d1 = np.hstack([d1, knownTerm.T])  
+                
         return C1, d1, actuation_polygons
         
     def linearized_cone_halfspaces_world(self, contactsNumber, ng, mu, normals, max_normal_force = 10000.0, saturate_max_normal_force = False):            

jet_leg/path_sequential_iterative_projection.py
@@ -32,97 +32,21 @@ from jet_leg.computational_dynamics import ComputationalDynamics
 from jet_leg.height_map import HeightMap
         
 class PathIterativeProjection:
+    def __init__(self):
+        self.compDyn = ComputationalDynamics()
         
-    def setup_path_iterative_projection(self, constraint_mode, contacts, comWF, trunk_mass, mu, normals):
+    def setup_path_iterative_projection(self, constraint_mode, contacts, comWF, trunk_mass, mu, normals, stanceLegs, stanceIndex, swingIndex, torque_limits):
         ''' parameters to be tuned'''
         g = 9.81
-        compDyn = ComputationalDynamics()
+        ng = 4;
         isOutOfWorkspace = False;
+        proj, eq, ineq, actuation_polygons = self.compDyn.setup_iterative_projection(constraint_mode, stanceLegs, comWF, contacts, normals, trunk_mass, ng, mu, torque_limits, False)
 
-        grav = array([0., 0., -g])
-        contactsNumber = np.size(contacts,0)
-        # Unprojected state is:
-        #
-        #     x = [f1_x, f1_y, f1_z, ... , f3_x, f3_y, f3_z]
-        Ex = np.zeros((0)) 
-        Ey = np.zeros((0))        
-        G = np.zeros((6,0))   
-        for j in range(0,contactsNumber):
-            r = contacts[j,:]
-            graspMatrix = compDyn.getGraspMatrix(r)[:,0:3]
-            Ex = hstack([Ex, -graspMatrix[4]])
-            Ey = hstack([Ey, graspMatrix[3]])
-            G = hstack([G, graspMatrix])            
+        (E, f), (A, b), (C, d) = proj, ineq, eq
+#        print 'D',b, d
+        if d.shape[0] == 1:
+            isOutOfWorkspace = True
             
-        E = vstack((Ex, Ey)) / (g)
-        f = zeros(2)
-        proj = (E, f)  # y = E * x + f
-        
-        # number of the equality constraints
-        m_eq = 6
-        
-        # see Equation (52) in "ZMP Support Areas for Multicontact..."
-        A_f_and_tauz = array([
-            [1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1]])
-        A = dot(A_f_and_tauz, G)
-        t = hstack([0, 0, g, 0])
-        #print A,t
-        eq = (A, t)  # A * x == t
-        
-        act_LF = np.zeros((0,1))
-        act_RF = np.zeros((0,1))
-        act_LH = np.zeros((0,1))
-        act_RH = np.zeros((0,1))
-        actuation_polygons = np.zeros((0,1))
-        # Inequality matrix for a contact force in local contact frame:
-        constr = Constraints()
-        #C_force = constr.linearized_cone_halfspaces(ng, mu)
-        # Inequality matrix for stacked contact forces in world frame:
-        if constraint_mode == 'ONLY_FRICTION':
-            C, d = constr.linearized_cone_halfspaces_world(contactsNumber, ng, mu, normals)
-            
-        elif constraint_mode == 'ONLY_ACTUATION':
-            #kin = Kinematics()
-            kin = HyQKinematics()
-            foot_vel = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]])
-            contactsFourLegs = np.vstack([contacts, np.zeros((4-contactsNumber,3))])
-            q, q_dot, J_LF, J_RF, J_LH, J_RH, isOutOfWorkspace = kin.inverse_kin(np.transpose(contactsFourLegs[:,0] - comWF[0]),
-                                                  np.transpose(foot_vel[:,0]),
-                                                    np.transpose(contactsFourLegs[:,1] - comWF[1]),
-                                                    np.transpose(foot_vel[:,1]),
-                                                    np.transpose(contactsFourLegs[:,2] - comWF[2]),
-                                                    np.transpose(foot_vel[:,2]))
-            J_LF, J_RF, J_LH, J_RH = kin.update_jacobians(q)
-
-            if isOutOfWorkspace:
-                C = np.zeros((0,0))
-                d = np.zeros((1,0))
-            else:
-                act_LF = constr.computeLegActuationPolygon(J_LF)
-                act_RF = constr.computeLegActuationPolygon(J_RF)
-                act_LH = constr.computeLegActuationPolygon(J_LH)
-                act_RH = constr.computeLegActuationPolygon(J_RH)            
-                ''' in the case of the IP alg. the contact force limits must be divided by the mass
-                because the gravito inertial wrench is normalized'''
-                
-                C = np.zeros((0,0))
-                d = np.zeros((1,0))
-                actuation_polygons = np.array([act_LF,act_RF,act_LH,act_RH])
-                for j in range (0,contactsNumber):
-                    hexahedronHalfSpaceConstraints, knownTerm = constr.hexahedron(actuation_polygons[j]/trunk_mass)
-                    C = block_diag(C, hexahedronHalfSpaceConstraints)
-                    d = hstack([d, knownTerm.T])
-                    
-                d = d.reshape(6*contactsNumber)    
-                #C = block_diag(c1, c2, c3, c4)
-                #d = np.vstack([e1, e2, e3, e4]).reshape(6*4)
-                #print C, d
-        
-        ineq = (C, d)  # C * x <= d
-        
         if isOutOfWorkspace:
             lp = 0
         else:
@@ -226,7 +150,7 @@ class PathIterativeProjection:
         return z
 
 
-    def compute_polygon_variable_constraint(self, constraint_mode, comWorldFrame, contactsWorldFrame, max_iter=50, solver=GLPK_IF_AVAILABLE):
+    def compute_polygon_variable_constraint(self, constraint_mode, comWorldFrame, contactsWorldFrame, stanceLegs, stanceIndex, swingIndex, torque_limits, max_iter=50, solver=GLPK_IF_AVAILABLE):
         """
         Expand a polygon iteratively.
     
@@ -260,7 +184,7 @@ class PathIterativeProjection:
             
         iterProj = PathIterativeProjection()
         
-        lp, actuation_polygons, isOutOfWorkspace = iterProj.setup_path_iterative_projection(constraint_mode, contactsWorldFrame, comWorldFrame, trunk_mass, mu, normals)
+        lp, actuation_polygons, isOutOfWorkspace = iterProj.setup_path_iterative_projection(constraint_mode, contactsWorldFrame, comWorldFrame, trunk_mass, mu, normals, stanceLegs, stanceIndex, swingIndex, torque_limits)
         
         if isOutOfWorkspace:
             return False
@@ -331,7 +255,7 @@ class PathIterativeProjection:
                     desired_com_line = self.line(comWF, comWF+desired_direction+np.array([random()*0.01,random()*0.01,0.0]))
                     new_point = self.two_lines_intersection(desired_com_line, actuation_region_edge)
                     intersection_points = np.vstack([intersection_points, new_point])
-                    print new_point
+#                    print new_point
                     
             #print intersection_points
             epsilon = 0.0001;
@@ -369,10 +293,10 @@ class PathIterativeProjection:
                     if alpha_vertices_y >= 0.0 and alpha_vertices_y <= 1.0:                   
                         points = np.vstack([points, new_point])
                         
-        print points
+#        print points
         return points, intersection_points
     
-    def find_vertex_along_path(self, constraint_mode, contacts, comWF, desired_direction, tolerance = 0.05, max_iteration_number = 10):
+    def find_vertex_along_path(self, constraint_mode, contacts, comWF, desired_direction, stanceLegs, stanceIndex, swingIndex, torque_limits, tolerance = 0.05, max_iteration_number = 10):
         final_points = np.zeros((0,2))
         newCoM = comWF
         comToStack = np.zeros((0,3))
@@ -381,7 +305,7 @@ class PathIterativeProjection:
         while_iter = 0
         while (np.amax(np.abs(increment))>tolerance) and (while_iter<max_iteration_number):
             comToStack = np.vstack([comToStack, newCoM])
-            polygon = self.compute_polygon_variable_constraint(constraint_mode, newCoM, contacts)
+            polygon = self.compute_polygon_variable_constraint(constraint_mode, newCoM, contacts, stanceLegs, stanceIndex, swingIndex, torque_limits)
             if polygon:
                 polygon.sort_vertices()
                 vertices_list = polygon.export_vertices()