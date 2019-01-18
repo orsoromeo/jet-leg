e190eb57530c69ce047fade892f1f81a0d415be7
Romeo Orsolino <romeo.orsolino@iit.it>
2019-01-18 14:57:24 +0100

    fixed bug in the fill polygons function

--- examples/communication.py
+++ examples/communication.py
@@ -34,49 +34,50 @@ sys.stderr = stderr
 
 
 class HyQSim(threading.Thread):
-    def __init__(self):  
-        
+    def __init__(self):
+
         threading.Thread.__init__(self)
-        
+
         self.clock_sub_name = 'clock'
         self.hyq_wbs_sub_name = "/hyq/robot_states"
         self.hyq_actuation_params_sub_name = "/hyq/debug"
         self.hyq_wbs = dict()
-        self.hyq_rcf_debug = StringDoubleArray()  
+        self.hyq_rcf_debug = StringDoubleArray()
         self.actuation_polygon_topic_name = "/hyq/actuation_polygon"
         self.support_region_topic_name = "/hyq/support_region"
         self.force_polygons_topic_name = "/hyq/force_polygons"
 
-
         self.sim_time  = 0.0
-        
+
     def run(self):
-        self.sub_clock = ros.Subscriber(self.clock_sub_name, Clock, callback=self._reg_sim_time, queue_size=1)
-        self.sub_actuation_params = ros.Subscriber(self.hyq_actuation_params_sub_name, StringDoubleArray, callback=self._reg_sim_rcf_debug, queue_size=1)
-        self.pub_polygon = ros.Publisher(self.actuation_polygon_topic_name, Polygon3D, queue_size=1)
-        self.pub_support_region = ros.Publisher(self.support_region_topic_name, Polygon3D, queue_size=1)
-        self.pub_force_polygons = ros.Publisher(self.force_polygons_topic_name, LegsPolygons, queue_size=1)
+        print "Run!"
+        self.sub_clock = ros.Subscriber(self.clock_sub_name, Clock, callback=self._reg_sim_time, queue_size=1000)
+        self.sub_actuation_params = ros.Subscriber(self.hyq_actuation_params_sub_name, StringDoubleArray,
+                                                   callback=self._reg_sim_rcf_debug, queue_size=1000)
+        self.pub_polygon = ros.Publisher(self.actuation_polygon_topic_name, Polygon3D, queue_size=10000)
+        self.pub_support_region = ros.Publisher(self.support_region_topic_name, Polygon3D, queue_size=1000)
+        self.pub_force_polygons = ros.Publisher(self.force_polygons_topic_name, LegsPolygons, queue_size=1000)
 
     def _reg_sim_time(self, time):
         self.sim_time = time.clock.secs + time.clock.nsecs/1000000000.0
 #        print("getting time")
-        
+
     def _reg_sim_wbs(self, msg):
         self.hyq_wbs = copy.deepcopy(msg)
 
     def _reg_sim_rcf_debug(self, msg):
 #        print 'new data received'
-        self.hyq_rcf_debug = copy.deepcopy(msg)  
-        
+        self.hyq_rcf_debug = copy.deepcopy(msg)
+
     def register_node(self):
         ros.init_node('sub_pub_node_python', anonymous=False)
 
     def deregister_node(self):
         ros.signal_shutdown("manual kill")
-        
+
     def get_sim_time(self):
         return self.sim_time
-        
+
     def get_sim_wbs(self):
         return self.hyq_wbs
 
@@ -84,44 +85,40 @@ class HyQSim(threading.Thread):
         output = LegsPolygons()
 #        output.polytopeName = name
         output.polygons = polygons
-        self.pub_force_polygons.publish(output) 
-        
+        self.pub_force_polygons.publish(output)
+
     def send_support_region(self, name, vertices):
         output = Polygon3D()
 #        output.names = name
         output.vertices = vertices
-        self.pub_support_region.publish(output) 
-        
+        self.pub_support_region.publish(output)
+
     def send_actuation_polygons(self, name, vertices, option_index, ack_optimization_done):
         output = Polygon3D()
 #        output.names = name
         output.vertices = vertices
-        output.option_index = option_index        
+        output.option_index = option_index
         output.ack_optimization_done = ack_optimization_done
-        self.pub_polygon.publish(output)    
-    
+        self.pub_polygon.publish(output)
+
     def send_simple_array(self, name, data):
         output = SimpleDoubleArray()
 #        output.name = name
         output.data = data
         self.pub_rcf_params.publish(output)
-    
+
     def fillPolygon(self, polygon):
-        
+
         num_actuation_vertices = np.size(polygon, 0)
-        point = Point()
-        point.x = polygon[0][0]
-        point.y = polygon[0][1]
-        point.z =  0.0 #is the centroidal frame
-        vertices1 = [point]
-        
+        vertices = []
+
         for i in range(0, num_actuation_vertices):
             point = Point()
             point.x = polygon[i][0]
             point.y = polygon[i][1]
             point.z = 0.0 #is the centroidal frame
-            vertices1 = np.hstack([vertices1, point])
-        return vertices1
+            vertices = np.hstack([vertices, point])
+        return vertices
 
 def talker():
     compDyn = ComputationalDynamics()
@@ -132,19 +129,15 @@ def talker():
     p.register_node()
     name = "Actuation_region"
     force_polytopes_name = "force_polytopes"
-    point = Point()
-    polygonVertex = Point()
-    polygon = Polygon3D()
 
     params = IterativeProjectionParameters()
     foothold_params = FootholdPlanningInterface()
     i = 0
 
-
-    point = Point()
-    polygonVertex = Point()
-    polygon = Polygon3D()    
     p.get_sim_wbs()
+    params.getParamsFromRosDebugTopic(p.hyq_rcf_debug)
+    foothold_params.getParamsFromRosDebugTopic(p.hyq_rcf_debug)
+    params.getFutureStanceFeet(p.hyq_rcf_debug)
    
 
     """ contact points """
@@ -254,7 +247,7 @@ def talker():
         # print 'optimization done',foothold_params.ack_optimization_done, ' ... ', foothold_params.optimization_started
         if foothold_params.optimization_started and not foothold_params.ack_optimization_done :
 
-            print foothold_params.footOptions
+            #print foothold_params.footOptions
             
             #chosen_foothold, actuationRegions = footHoldPlanning.selectMaximumFeasibleArea(foothold_params, params)
 #            print 'current swing ',params.actual_swing
@@ -264,14 +257,16 @@ def talker():
                 foothold_params.option_index = -1
             else:
                 print 'min radius ', foothold_params.minRadius, 'residual radius ', stackedResidualRadius
-                print 'feet options', foothold_params.footOptions
+                #print 'feet options', foothold_params.footOptions
                 print 'final index', foothold_params.option_index, 'index list', mapFootHoldIdxToPolygonIdx
                 
-            foothold_params.ack_optimization_done = True    
+            foothold_params.ack_optimization_done = 1
         
             #this sends the data back to ros that contains the foot hold choice (used for stepping) and the corrspondent region (that will be used for com planning TODO update with the real footholds)
-            if (actuationRegions is not False) and (np.size(actuationRegions) is not 0):                
+            if (actuationRegions is not False) and (np.size(actuationRegions) is not 0):
+                print 'sending actuation region'
                 p.send_actuation_polygons(name, p.fillPolygon(actuationRegions[-1]), foothold_params.option_index, foothold_params.ack_optimization_done)
+                print actuationRegions[-1]
             else:
                 #if it cannot compute anything it will return the frictin region
                 p.send_actuation_polygons(name, p.fillPolygon(frictionRegion), foothold_params.option_index, foothold_params.ack_optimization_done)
