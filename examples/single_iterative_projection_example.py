899b81046d644d403dd2dce89c075f93c590b3fa
aabdalla <abdelrahman.abdalla@iit.it>
2019-09-27 14:54:41 +0200

    Addd some documentation to "single_iterative_projection_example.py"

--- examples/single_iterative_projection_example.py
+++ examples/single_iterative_projection_example.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct 18 11:05:06 2018
-
-@author: rorsolino
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Tue Jun 12 10:54:31 2018
 
 @author: Romeo Orsolino
@@ -32,27 +25,29 @@ from jet_leg.arrow3D import Arrow3D
         
 plt.close('all')
 math = Math()
-# number of contacts
-#nc = 3
-# number of generators, i.e. rays used to linearize the friction cone
+
+''' number of generators, i.e. rays/edges used to linearize the friction cone '''
 ng = 4
 
-# ONLY_ACTUATION, ONLY_FRICTION or FRICTION_AND_ACTUATION
-
-constraint_mode_IP = ['ONLY_FRICTION',
-                      'ONLY_ACTUATION',
+'''
+possible constraints for each foot:
+ ONLY_ACTUATION = only joint-torque limits are enforces
+ ONLY_FRICTION = only friction cone constraints are enforced
+ FRICTION_AND_ACTUATION = both friction cone constraints and joint-torque limits
+'''
+constraint_mode_IP = ['FRICTION_AND_ACTUATION',
+                      'FRICTION_AND_ACTUATION',
                       'FRICTION_AND_ACTUATION',
                       'FRICTION_AND_ACTUATION']
 useVariableJacobian = False
 # number of decision variables of the problem
 #n = nc*6
 comWF = np.array([1.0, 0.0, 0.0])
-# contact positions
-""" contact points """
 
-LF_foot = np.array([1.3, 0.2, -0.5])
+""" contact points in the World Frame"""
+LF_foot = np.array([1.3, 0.2, -0.6])
 RF_foot = np.array([1.3, -0.2, -0.5])
-LH_foot = np.array([0.7, 0.2, -0.5])
+LH_foot = np.array([0.7, 0.2, -0.4])
 RH_foot = np.array([0.7, -0.2, -0.5])
 
 contacts = np.vstack((LF_foot,RF_foot,LH_foot,RH_foot))
@@ -63,28 +58,38 @@ contacts = np.vstack((LF_foot,RF_foot,LH_foot,RH_foot))
 ''' parameters to be tuned'''
 g = 9.81
 trunk_mass = 85.
-mu = 0.9
+mu = 0.5
 
+''' stanceFeet vector contains 1 is the foot is on the ground and 0 if it is in the air'''
 stanceFeet = [1,1,1,1]
+
 randomSwingLeg = random.randint(0,3)
 print 'Swing leg', randomSwingLeg
+# stanceFeet[randomSwingLeg] = 0
 print 'stanceLegs ' ,stanceFeet
 
+''' now I define the normals to the surface of the contact points. By default they are all vertical now'''
 axisZ= array([[0.0], [0.0], [1.0]])
 
-n1 = np.transpose(np.transpose(math.rpyToRot(0.0,0.0,0.0)).dot(axisZ))
-n2 = np.transpose(np.transpose(math.rpyToRot(0.0,0.0,0.0)).dot(axisZ))
-n3 = np.transpose(np.transpose(math.rpyToRot(0.0,0.0,0.0)).dot(axisZ))
-n4 = np.transpose(np.transpose(math.rpyToRot(0.0,0.0,0.0)).dot(axisZ))
+n1 = np.transpose(np.transpose(math.rpyToRot(0.0,0.0,0.0)).dot(axisZ))  # LF
+n2 = np.transpose(np.transpose(math.rpyToRot(0.0,0.0,0.0)).dot(axisZ))  # RF
+n3 = np.transpose(np.transpose(math.rpyToRot(0.0,0.0,0.0)).dot(axisZ))  # LH
+n4 = np.transpose(np.transpose(math.rpyToRot(0.0,0.0,0.0)).dot(axisZ))  # RH
 normals = np.vstack([n1, n2, n3, n4])
 
-LF_tau_lim = [50.0, 100.0, 100.0]
+''' torque limits for each leg (this code assumes a hyq-like design, i.e. three joints per leg)
+HAA = Hip Abduction Adduction
+HFE = Hip Flextion Extension
+KFE = Knee Flextion Extension
+'''
+LF_tau_lim = [50.0, 100.0, 100.0] # HAA, HFE, KFE
 RF_tau_lim = [50.0, 100.0, 100.0]
 LH_tau_lim = [50.0, 100.0, 100.0]
 RH_tau_lim = [50.0, 100.0, 100.0]
 torque_limits = np.array([LF_tau_lim, RF_tau_lim, LH_tau_lim, RH_tau_lim])
 
-extForceW = np.array([0.0,0.0, 700.0])
+''' extForceW is an optional external pure force (no external torque for now) applied on the CoM of the robot.'''
+extForceW = np.array([0.0,0.0, 0.0])
 #
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -109,10 +114,15 @@ params.setConstraintModes(constraint_mode_IP)
 params.setContactNormals(normals)
 params.setFrictionCoefficient(mu)
 params.setNumberOfFrictionConesEdges(ng)
-params.setTotalMass(trunk_mass + extForceW[2]/9.81)
-params.externalForceWF = extForceW
+params.setTotalMass(trunk_mass + extForceW[2]/g) # here I am assuming that the external force is due to an extra load and I want everything in the same units (Kilos)
+params.externalForceWF = extForceW  # I don't think that params.externalForceWF is actually used anywhere at the moment
 
-''' compute iterative projection '''
+''' compute iterative projection 
+Outputs of "iterative_projection_bretl" are:
+IP_points = resulting 2D vertices
+actuation_polygons = these are the vertices of the 3D force polytopes (one per leg)
+computation_time = how long it took to compute the iterative projection
+'''
 IP_points, actuation_polygons, computation_time = comp_dyn.iterative_projection_bretl(params)
 
 #print 'actuation polygons', actuation_polygons
@@ -120,9 +130,9 @@ IP_points, actuation_polygons, computation_time = comp_dyn.iterative_projection_
 ''' plotting Iterative Projection points '''
 plotter = Plotter()
 scaling_factor = 2000
-if constraint_mode_IP == 'ONLY_ACTUATION':
+for j in range(0,4):
     plotter.plot_polygon(np.transpose(IP_points))
-    for j in range(0,4):
+    if (constraint_mode_IP[j] == 'ONLY_ACTUATION') or (constraint_mode_IP[j] == 'FRICTION_AND_ACTUATION'):
 #        print 'a',actuation_polygons[j], contacts[j,:]
         plotter.plot_actuation_polygon(ax, actuation_polygons[j], contacts[j,:], scaling_factor)
 
