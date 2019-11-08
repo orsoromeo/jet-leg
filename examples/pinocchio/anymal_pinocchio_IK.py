6c733dd3dd907a3cffd64fc3ec41cbe84b538a5b
Romeo Orsolino <rorsolino@robots.ox.ac.uk>
2019-11-08 14:54:42 +0000

    added example of anymals's IK in pinocchio

---
+++ examples/pinocchio/anymal_pinocchio_IK.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+from jet_leg.robot.anymal_kinematics import anymalKinematics
+from pinocchio.utils import *
+
+import time
+
+LF_foot = np.array([0.3735, 0.33, -0.5])
+RF_foot = np.array([0.3735, -0.33, -0.5])
+LH_foot = np.array([-0.3735, 0.33, -0.5])
+RH_foot = np.array([-0.3735, -0.33, -0.5])
+feet_pos_des = np.vstack((LF_foot, RF_foot, LH_foot, RH_foot))
+
+print(feet_pos_des)
+
+kin = anymalKinematics()
+start_time = time.time()
+q, legJacobians = kin.anymalFixedBaseInverseKinematics(feet_pos_des)
+print('total time is ',time.time()-start_time)
+print('q is:', q.T)
+print('\nresult: %s' % q.flatten().tolist())
+
