kinematics interface works but there is still some wrong value in the jacobians of HyQ

jet_leg/dynamics/computational_dynamics.py
@@ -10,7 +10,7 @@ import numpy as np
 from numpy import array, dot, eye, hstack, vstack, zeros
 from scipy.spatial import ConvexHull
 from jet_leg.constraints.constraints import Constraints
-from jet_leg.robots.hyq.hyq_kinematics import HyQKinematics
+from jet_leg.kinematics.kinematics_interface import KinematicsInterface
 from jet_leg.maths.math_tools import Math
 from jet_leg.maths.geometry import Geometry
 from cvxopt import matrix, solvers
@@ -22,8 +22,8 @@ class ComputationalDynamics:
         self.robotName = robot_name
         self.geom = Geometry()
         self.math = Math()
-        self.constr = Constraints(self.robotName)
-        self.kin = HyQKinematics(self.robotName)
+        self.kin = KinematicsInterface(self.robotName)
+        self.constr = Constraints(self.kin)
         self.ineq = ([],[])
         self.eq = ([],[])
 
@@ -96,7 +96,25 @@ class ComputationalDynamics:
 #        print 'mass ', robotMass
 #        print A,t
         eq = (A, t)  # A * x == t
-        
+
+        #we are static so we set to zero
+        foot_vel = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]])
+
+        contactsWF = iterative_projection_params.getContactsPosWF()
+        comPositionWF = iterative_projection_params.getCoMPosWF()
+        comPositionBF = iterative_projection_params.getCoMPosBF()
+        rpy = iterative_projection_params.getOrientation()
+        #compute the contacs in the base frame for the inv kineamtics
+        contactsBF = np.zeros((4,3))
+
+        for j in np.arange(0, 4):
+            j = int(j)
+            contactsBF[j,:]= np.add( np.dot(self.math.rpyToRot(rpy[0], rpy[1], rpy[2]), (contactsWF[j,:] - comPositionWF)), comPositionBF)
+
+        q = self.kin.inverse_kin(contactsBF, foot_vel)
+        #self.kin.update_homogeneous(q)
+        J_LF, J_RF, J_LH, J_RH, isIKoutOfWorkSpace = self.kin.get_jacobians()
+
         C, d, isIKoutOfWorkSpace, actuation_polygons = self.constr.getInequalities(iterative_projection_params)
         ineq = (C, d)    
         return proj, eq, ineq, actuation_polygons, isIKoutOfWorkSpace
@@ -347,9 +365,8 @@ class ComputationalDynamics:
             contactsBF[j, :] = contactsPosWF[j, :] - comWorldFrame
 
         foot_vel = np.vstack([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
-        q = self.kin.inverse_kin(contactsBF, foot_vel, self.robotName)
-        J_LF, J_RF, J_LH, J_RH, isOutOfWorkspace = self.kin.get_jacobians(self.robotName)
-
+        q = self.kin.inverse_kin(contactsBF, foot_vel)
+        J_LF, J_RF, J_LH, J_RH, isOutOfWorkspace = self.kin.get_jacobians()
 
         if (not isOutOfWorkspace):
             G, h, isIKoutOfWorkSpace, LP_actuation_polygons = self.constr.getInequalities(LPparams)

jet_leg/robots/hyq/hyq_kinematics.py
@@ -8,19 +8,12 @@ import numpy as np
 
 from jet_leg.robots.dog_interface import DogInterface
 from jet_leg.dynamics.rigid_body_dynamics import RigidBodyDynamics
-from jet_leg.robots.anymal.anymal_kinematics import anymalKinematics
-from jet_leg.robots.hyqreal.hyqreal_kinematics import hyqrealKinematics
 
 class HyQKinematics:
-    def __init__(self, robot_name):
+    def __init__(self):
         
         self.dog = DogInterface()
         self.rbd = RigidBodyDynamics()
-        self.robotName = robot_name
-        if self.robotName == 'anymal':
-            self.anymalKin = anymalKinematics()
-        elif self.robotName == 'hyqreal':
-            self.hyqrealKin = hyqrealKinematics()
 
         self.upperLegLength = 0.35;
         self.lowerLegLength = 0.341;
@@ -800,14 +793,9 @@ class HyQKinematics:
 #        print self.fr_trunk_J_LF_foot, self.fr_trunk_J_RF_foot, self.fr_trunk_J_LH_foot, self.fr_trunk_J_RH_foot
         return self.fr_trunk_J_LF_foot[3:6,:] , self.fr_trunk_J_RF_foot[3:6,:], self.fr_trunk_J_LH_foot[3:6,:], self.fr_trunk_J_RH_foot[3:6,:]
 
-    def get_jacobians(self, robot_name):
-        self.robotName = robot_name
-        if self.robotName == 'hyq':
-            return self.fr_trunk_J_LF_foot[3:6,:] , self.fr_trunk_J_RF_foot[3:6,:], self.fr_trunk_J_LH_foot[3:6,:], self.fr_trunk_J_RH_foot[3:6,:], self.isOutOfWorkSpace
-        elif self.robotName == 'hyqreal':
-            return self.hyqrealKin.getLegJacobians()
-        elif self.robotName == 'anymal':
-            return self.anymalKin.getLegJacobians()
+    def getLegJacobians(self):
+        return self.fr_trunk_J_LF_foot[3:6,:] , self.fr_trunk_J_RF_foot[3:6,:], self.fr_trunk_J_LH_foot[3:6,:], self.fr_trunk_J_RH_foot[3:6,:], self.isOutOfWorkSpace
+
 
     def forward_kin(self, q):
         LF_foot = self.fr_trunk_Xh_LF_foot[0:3,3]
@@ -965,20 +953,14 @@ class HyQKinematics:
             
         return q_leg
     
-    def inverse_kin(self, contactsBF, foot_vel, robot_name):
-        self.robotName = robot_name
+    def fixedBaseInverseKinematics(self, contactsBF, foot_vel):
         q = []
-        if self.robotName == 'hyq':
-            for legID in self.dog.legs:
-                q_leg = self.leg_inverse_kin(legID, contactsBF[legID,:], foot_vel[legID,:])
-                q = np.hstack([q, q_leg])
-            return q
+        for legID in self.dog.legs:
+            q_leg = self.leg_inverse_kin(legID, contactsBF[legID,:], foot_vel[legID,:])
+            q = np.hstack([q, q_leg])
 
-        elif self.robotName == 'hyqreal':
-            q, jac = self.hyqrealKin.fixedBaseInverseKinematics(contactsBF)
-            return q
+        self.update_homogeneous(q)
+        self.update_jacobians(q)
+        return q
 
-        elif self.robotName == 'anymal':
-            q, jac = self.anymalKin.fixedBaseInverseKinematics(contactsBF)
-            return q
         