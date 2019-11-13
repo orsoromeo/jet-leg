00f059e79513deddaab6a351201690acd4f46ef5
Siddhant Gangapurwala <siddhant.gangapurwala@eng.ox.ac.uk>
2019-11-13 11:47:51 +0000

    updated for refactored jet-leg

--- jet_leg/robots/anymal/anymal_kinematics.py
+++ jet_leg/robots/anymal/anymal_kinematics.py
@@ -1,12 +1,18 @@
+import os
+
 import pinocchio
-from pinocchio.utils import *
 from pinocchio.robot_wrapper import RobotWrapper
-import matplotlib.pyplot as plt
+from pinocchio.utils import *
+
 
 class anymalKinematics():
     def __init__(self):
-        self.PKG = '/opt/openrobots/share/example-robot-data'
-        self.URDF = '/opt/openrobots/share/example-robot-data/anymal_b_simple_description/robots/anymal.urdf'
+        self.PKG = os.path.dirname(os.path.abspath(__file__)) + '/../../../resources/urdfs/anymal/'
+        self.URDF = self.PKG + 'urdf/anymal_boxy.urdf'
+        if self.PKG is None:
+            self.robot = RobotWrapper.BuildFromURDF(self.URDF)
+        else:
+            self.robot = RobotWrapper.BuildFromURDF(self.URDF, [self.PKG])
         self.robot = RobotWrapper.BuildFromURDF(self.URDF, [self.PKG])
         self.model = self.robot.model
         self.data = self.robot.data
@@ -30,13 +36,13 @@ class anymalKinematics():
         elif frame_name == self.urdf_foot_name_rh:
             idx = 9
 
-        #print ('index is',idx)
+        # print ('index is',idx)
         return idx
 
     def footInverseKinematicsFixedBase(self, foot_pos_des, frame_name):
         frame_id = self.model.getFrameId(frame_name)
         blockIdx = self.getBlockIndex(frame_name)
-        anymal_q0 = np.vstack([-0.1, 0.7, -1., -0.1, -0.7, 1., 0.1, 0.7,-1., 0.1,-0.7, 1.])
+        anymal_q0 = np.vstack([-0.1, 0.7, -1., -0.1, -0.7, 1., 0.1, 0.7, -1., 0.1, -0.7, 1.])
         q = anymal_q0
         eps = 0.005
         IT_MAX = 200
@@ -49,10 +55,10 @@ class anymalKinematics():
             pinocchio.forwardKinematics(self.model, self.data, q)
             pinocchio.framesForwardKinematics(self.model, self.data, q)
             foot_pos = self.data.oMf[frame_id].translation
-            #err = np.hstack([err, (foot_pos - foot_pos_des)])
-            #e = err[:,-1]
-            #print foot_pos_des[0], foot_pos[[0]], foot_pos[[0]] - foot_pos_des[0]
-            #e = foot_pos - foot_pos_des
+            # err = np.hstack([err, (foot_pos - foot_pos_des)])
+            # e = err[:,-1]
+            # print foot_pos_des[0], foot_pos[[0]], foot_pos[[0]] - foot_pos_des[0]
+            # e = foot_pos - foot_pos_des
             e[0] = foot_pos[[0]] - foot_pos_des[0]
             e[1] = foot_pos[[1]] - foot_pos_des[1]
             e[2] = foot_pos[[2]] - foot_pos_des[2]
@@ -61,21 +67,23 @@ class anymalKinematics():
             J_lin = J[:3, :]
 
             if np.linalg.norm(e) < eps:
-                #print("IK Convergence achieved!")
+                # print("IK Convergence achieved!")
                 IKsuccess = True
                 break
             if i >= IT_MAX:
-                print("\n Warning: the iterative algorithm has not reached convergence to the desired precision. Error is: ", np.linalg.norm(e))
+                print(
+                    "\n Warning: the iterative algorithm has not reached convergence to the desired precision. Error is: ",
+                    np.linalg.norm(e))
                 IKsuccess = False
                 break
-            #print J_lin
+            # print J_lin
             v = - np.linalg.pinv(J_lin) * e
             q = pinocchio.integrate(self.model, q, v * DT)
             i += 1
-            #print i
+            # print i
 
-        q_leg = q[blockIdx:blockIdx+3]
-        J_leg = J_lin[:,blockIdx:blockIdx+3]
+        q_leg = q[blockIdx:blockIdx + 3]
+        J_leg = J_lin[:, blockIdx:blockIdx + 3]
         return q_leg, J_leg, err, IKsuccess
 
     def fixedBaseInverseKinematics(self, feetPosDes):
@@ -86,19 +94,23 @@ class anymalKinematics():
         self.RH_foot_jac = []
         leg_ik_success = np.zeros((4))
 
-        f_p_des = np.array(feetPosDes[0,:]).T
-        q_LF, self.LF_foot_jac, err, leg_ik_success[0] = self.footInverseKinematicsFixedBase(f_p_des, self.urdf_foot_name_lf)
+        f_p_des = np.array(feetPosDes[0, :]).T
+        q_LF, self.LF_foot_jac, err, leg_ik_success[0] = self.footInverseKinematicsFixedBase(f_p_des,
+                                                                                             self.urdf_foot_name_lf)
 
         f_p_des = np.array(feetPosDes[2, :]).T
-        q_LH, self.LH_foot_jac, err, leg_ik_success[2] = self.footInverseKinematicsFixedBase(f_p_des, self.urdf_foot_name_lh)
+        q_LH, self.LH_foot_jac, err, leg_ik_success[2] = self.footInverseKinematicsFixedBase(f_p_des,
+                                                                                             self.urdf_foot_name_lh)
 
         f_p_des = np.array(feetPosDes[1, :]).T
-        q_RF, self.RF_foot_jac, err, leg_ik_success[1] = self.footInverseKinematicsFixedBase(f_p_des, self.urdf_foot_name_rf)
+        q_RF, self.RF_foot_jac, err, leg_ik_success[1] = self.footInverseKinematicsFixedBase(f_p_des,
+                                                                                             self.urdf_foot_name_rf)
 
         f_p_des = np.array(feetPosDes[3, :]).T
-        q_RH, self.RH_foot_jac, err, leg_ik_success[3] = self.footInverseKinematicsFixedBase(f_p_des, self.urdf_foot_name_rh)
+        q_RH, self.RH_foot_jac, err, leg_ik_success[3] = self.footInverseKinematicsFixedBase(f_p_des,
+                                                                                             self.urdf_foot_name_rh)
 
-        self.ik_success =  bool(leg_ik_success[0] and leg_ik_success[1] and leg_ik_success[2] and leg_ik_success[3])
+        self.ik_success = bool(leg_ik_success[0] and leg_ik_success[1] and leg_ik_success[2] and leg_ik_success[3])
 
         if self.ik_success is False:
             print('Warning, IK failed. Jacobian is singular')
@@ -111,4 +123,3 @@ class anymalKinematics():
     def getLegJacobians(self):
         isOutOfWS = not self.ik_success
         return self.LF_foot_jac, self.RF_foot_jac, self.LH_foot_jac, self.RH_foot_jac, isOutOfWS
-
