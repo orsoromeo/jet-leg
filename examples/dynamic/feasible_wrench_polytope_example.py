afde48b64ae791dda538c436fd27a404002fb0ed
Romeo Orsolino <rorsolino@robots.ox.ac.uk>
2019-12-18 09:25:17 +0000

    fwp feasibility seems to work only when comWF is in the origin

--- examples/dynamic/feasible_wrench_polytope_example.py
+++ examples/dynamic/feasible_wrench_polytope_example.py
@@ -40,7 +40,7 @@ constraint_mode_IP = ['FRICTION_AND_ACTUATION',
 
 # number of decision variables of the problem
 #n = nc*6
-comWF = np.array([0., 0., .0])
+comWF = np.array([1., 1., .0])
 comWFvel = np.array([.0, .0, .0])
 comWF_lin_acc = np.array([-.0, -0.0, .0])
 comWF_ang_acc = np.array([.0, .0, .0])
@@ -145,8 +145,8 @@ plt.arrow(comWF[0], comWF[1], inertialForce[0], inertialForce[1], head_width=0.0
 plt.arrow(comWF[0] + inertialForce[0], comWF[1] + inertialForce[1], extForce[0], extForce[1], head_width=0.01,
               head_length=0.01, fc='blue', ec='blue', label='external force')
 
-plt.scatter([comWF[0], inertialForce[0]], [comWF[1], inertialForce[1]], color='k', marker= '^', label='inertial acceleration')
-plt.scatter([comWF[0], inertialForce[0]], [comWF[1], inertialForce[1]], color='b', marker= '^', label='external force')
+plt.scatter([comWF[0], comWF[0]+inertialForce[0]], [comWF[1], comWF[1]+inertialForce[1]], color='k', marker= '^', label='inertial acceleration')
+plt.scatter([comWF[0], comWF[0]+inertialForce[0]], [comWF[1], comWF[1]+inertialForce[1]], color='b', marker= '^', label='external force')
 
 if isStaticallyStable:
     plt.plot(comWF[0],comWF[1],'g',markersize=20, marker= '^', label='CoM (static check)')
--- jet_leg/dynamics/feasible_wrench_polytope.py
+++ jet_leg/dynamics/feasible_wrench_polytope.py
@@ -30,7 +30,6 @@ class FeasibleWrenchPolytope():
         com_pos_WF = fwp_params.getCoMPosWF()
         static_angular = np.cross(com_pos_WF, static_linear)
         extForce = fwp_params.externalCentroidalWrench[0:3]
-        print extForce
         inertial_linear = fwp_params.getTotalMass()*fwp_params.getCoMLinAcc() + extForce
         inertial_angular = fwp_params.getCoMLinAcc() # TODO: add the inertial matrix here
         linear_aggr_wrench = inertial_linear - static_linear
@@ -40,81 +39,67 @@ class FeasibleWrenchPolytope():
         return w_gi
 
     def computeAngularPart(self, fwp_params, forcePolygonsVertices):
-        print "compute ang part",np.shape(forcePolygonsVertices[0]),
 
         contactsWF = fwp_params.getContactsPosWF().T
         wrenchPolytopes = []
         stanceLegs = fwp_params.getStanceFeet()
         stanceIndex = fwp_params.getStanceIndex(stanceLegs)
+        print stanceIndex
         contactsNumber = np.sum(stanceLegs)
         for i in range(0, contactsNumber):
             index = int(stanceIndex[i])
+            print index
             footPosWF = contactsWF[:, index]
             currentPolytope = np.array(forcePolygonsVertices[index])
-            print "current poly", currentPolytope[:,0]
             dim, numOfVertices = np.shape(currentPolytope)
             angularPart = np.zeros((3, numOfVertices))
-            for j in np.arange(0, 8):
+            for j in np.arange(0, numOfVertices):
                 linear = currentPolytope[:, j]
                 angularPart[:, j] = np.cross(footPosWF, linear)
 
             sixDpoly = np.vstack([currentPolytope, angularPart])
-            print np.shape(sixDpoly)
             wrenchPolytopes.append(sixDpoly)
 
         return wrenchPolytopes
 
-    def computedPolytopeConeIntersection(self, forcePolytopes):
+    def computedPolytopeConeIntersection(self, fwp_params, forcePolytopes):
         hs = forcePolytopes.getHalfspaces()
         result = []
         feasible_point = np.array([0.0, 0.0, 1.0])
-        h = np.array([hs[0]])
-        print np.shape(h[0])
-        intersection = HalfspaceIntersection(h[0], feasible_point)
-        vx1 = zip(*intersection.intersections)
-        result.append(vx1)
 
-        h = np.array([hs[1]])
-        intersection = HalfspaceIntersection(h[0], feasible_point)
-        vx2 = zip(*intersection.intersections)
-        result.append(vx2)
-
-        h = np.array([hs[2]])
-        intersection = HalfspaceIntersection(h[0], feasible_point)
-        vx3 = zip(*intersection.intersections)
-        result.append(vx3)
-
-        h = np.array([hs[3]])
-        intersection = HalfspaceIntersection(h[0], feasible_point)
-        vx4 = zip(*intersection.intersections)
-        result.append(vx4)
+        stanceLegs = fwp_params.getStanceFeet()
+        stanceIndex = fwp_params.getStanceIndex(stanceLegs)
+        contactsNumber = np.sum(stanceLegs)
+        for i in range(0, contactsNumber):
+            index = int(stanceIndex[i])
+            h = np.array([hs[index]])
+            intersection = HalfspaceIntersection(h[0], feasible_point)
+            vx = zip(*intersection.intersections)
+            result.append(vx)
 
         return result
 
     def computeFeasibleWrenchPolytopeVRep(self, fwp_params, forcePolytopes):
 
         forcePolygonsVertices = forcePolytopes.getVertices()
-        intersectionVx = self.computedPolytopeConeIntersection(forcePolytopes)
-        #intersectionVx = forcePolygonsVertices # only for debugging
+        #intersectionVx = self.computedPolytopeConeIntersection(fwp_params, forcePolytopes)
+        intersectionVx = forcePolygonsVertices # only for debugging
 
         actuationWrenchPolytopesVRep = self.computeAngularPart(fwp_params, intersectionVx)
-
         stanceLegs = fwp_params.getStanceFeet()
-        stanceIndex = fwp_params.getStanceIndex(stanceLegs)
         contactsNumber = np.sum(stanceLegs)
-
         polytopesInContact = []
         for i in range(0, contactsNumber):
-            index = int(stanceIndex[i])
-            polytopesInContact.append(actuationWrenchPolytopesVRep[index])
+            polytopesInContact.append(actuationWrenchPolytopesVRep[i])
 
         tmpSum = np.array(polytopesInContact[0])
-        i = 0
         for j in np.arange(0, contactsNumber - 1):
             nextPolygon = np.array(polytopesInContact[j + 1])
-            #print "next", nextPolygon
+            print np.shape(nextPolygon)
             tmpSum = self.vProj.minksum(tmpSum, nextPolygon)
 
+        print np.shape(tmpSum)
         currentPolygonSum = self.vProj.convex_hull(tmpSum)
+        print np.shape(currentPolygonSum)
 
         return currentPolygonSum
