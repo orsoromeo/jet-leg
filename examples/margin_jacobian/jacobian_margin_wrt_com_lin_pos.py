fix sign in computation of margin derivatives

examples/margin_jacobian/jacobian_margin_wrt_com_lin_pos.py
@@ -88,9 +88,6 @@ def computeAnalyticMarginAndDerivatives(stanceFeet):
     contactsWF = np.vstack((LF_foot, RF_foot, LH_foot, RH_foot))
     params.setContactsPosWF(contactsWF)
 
-
-    start = time.time()
-
     params.useContactTorque = True
     params.useInstantaneousCapturePoint = True
     params.externalCentroidalWrench = extCentroidalWrench
@@ -108,18 +105,8 @@ def computeAnalyticMarginAndDerivatives(stanceFeet):
     params_com_x = deepcopy(params)
     params_com_y = deepcopy(params)
     params_com_z = deepcopy(params)
-    params_com_vel_x = deepcopy(params)
-    params_com_vel_y = deepcopy(params)
-    params_com_vel_z = deepcopy(params)
-    params_com_acc_x = deepcopy(params)
-    params_com_acc_y = deepcopy(params)
-    params_com_acc_z = deepcopy(params)
-    params_base_roll = deepcopy(params)
-    params_base_pitch = deepcopy(params)
 
     jac = Jacobians(robot_name)
-    comp_geom = ComputationalGeometry()
-    com = CoM(params)
 
     delta_pos_range = 0.39
     delta_pos_range_z = 0.2
@@ -134,8 +121,6 @@ def computeAnalyticMarginAndDerivatives(stanceFeet):
     pos_margin_y, jac_com_pos_y = jac.plotMarginAndJacobianWrtComPosition(params_com_y,delta_pos_range_vec_y, 1) # dm / dy
     pos_margin_z, jac_com_pos_z = jac.plotMarginAndJacobianWrtComPosition(params_com_z,delta_pos_range_vec_z, 2) # dm / dz
 
-    delta_vel_range = 3.0
-    delta_vel_range_vec = np.linspace(-delta_vel_range/2.0, delta_vel_range/2.0, num_of_tests)
     return pos_margin_x, jac_com_pos_x, pos_margin_y, jac_com_pos_y, pos_margin_z, jac_com_pos_z, delta_pos_range_vec_x, delta_pos_range_vec_y, delta_pos_range_vec_z
 
 
@@ -190,7 +175,7 @@ learnedMargin.plot_learned_margin('com_jacobian_1111stance.txt')
 
 fig2 = plt.figure(2)
 fig2.suptitle("Analytic vs. Learned stability margin\n 3 stance feet (0111)")
-contacts = [1, 1, 1, 0]
+contacts = [0, 1, 1, 1]
 mx, jx, my, jy, mz, jz, vx, vy, vz = computeAnalyticMarginAndDerivatives(contacts)
 plotAnalyticMarginAndDerivatives(mx, jx, my, jy, mz, jz, vx, vy, vz)
 learnedMargin.plot_learned_margin('com_jacobian_0111stance.txt')

jet_leg/optimization/jacobians.py
@@ -43,12 +43,12 @@ class Jacobians:
 
         for j in np.arange(0, 3):
             feet_pos_dim = copy.deepcopy(default[:, j])
-            feet_pos_dim -= self.delta
+            feet_pos_dim += self.delta
             feet_pos_WF_plus = copy.deepcopy(default)
             feet_pos_WF_plus[:, j] = feet_pos_dim
             isPointFeasible, margin1 = self.computeComMargin(params, feet_pos_WF_plus)
             feet_pos_dim = copy.deepcopy(default[:, j])
-            feet_pos_dim += self.delta
+            feet_pos_dim -= self.delta
             feet_pos_WF_minus = copy.deepcopy(default)
             feet_pos_WF_minus[:, j] = feet_pos_dim
             isPointFeasible, margin2 = self.computeComMargin(params, feet_pos_WF_minus)
@@ -251,7 +251,7 @@ class Jacobians:
         for delta in com_pos_range:
             """ contact points in the World Frame"""
             contactsWF = copy.deepcopy(default_feet_pos_WF)
-            contactsWF[:, dim_to_check] -= delta
+            contactsWF[:, dim_to_check] += delta
             isPointFeasible, margin[count] = self.computeComMargin(params, contactsWF)
             jac_com_pos[:, count] = self.computeComPosJacobian(params, contactsWF)
 