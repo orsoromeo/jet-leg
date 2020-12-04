[RigidBodyDynamics] fix sign error on the inertial wrenches

examples/margin_jacobian/jacobian_margin_wrt_com_lin_pos.py
@@ -13,7 +13,7 @@ from jet_leg.computational_geometry.math_tools import Math
 from jet_leg.dynamics.computational_dynamics import ComputationalDynamics
 from jet_leg.computational_geometry.iterative_projection_parameters import IterativeProjectionParameters
 from jet_leg.optimization.jacobians import Jacobians
-from plot_learned_com_margin_jac import LearnedMargin
+from plot_learned_com_pos_margin_jac import LearnedMargin
 
 import matplotlib.pyplot as plt
 
@@ -41,7 +41,7 @@ def computeAnalyticMarginAndDerivatives(stanceFeet, robot):
 
     delta_pos_range = 0.79
     delta_pos_range_z = 0.4
-    dx = 0.04
+    dx = 0.02
     num_of_tests = delta_pos_range/dx
     delta_pos_range_vec_x = np.linspace(-delta_pos_range/2.0, delta_pos_range/2.0, num_of_tests)
     delta_pos_range_vec_y = np.linspace(-delta_pos_range/2.0, delta_pos_range/2.0, num_of_tests)
@@ -103,7 +103,7 @@ fig1.suptitle("Analytic vs. Learned stability margin\n 4 stance feet (1111)")
 contacts = [1, 1, 1, 1]
 mx, jx, my, jy, mz, jz, vx, vy, vz = computeAnalyticMarginAndDerivatives(contacts, robot_name)
 plotAnalyticMarginAndDerivatives(mx, jx, my, jy, mz, jz, vx, vy, vz)
-learnedMargin.plot_learned_margin('com_jacobian_anymal_c_1111stance.txt')
+learnedMargin.plot_learned_margin('1111stance.txt')
 learnedMargin.set_plot_properties()
 
 fig2 = plt.figure(2)
@@ -111,7 +111,7 @@ fig2.suptitle("Analytic vs. Learned stability margin\n LF foot in stance (0111)"
 contacts = [0, 1, 1, 1]
 mx, jx, my, jy, mz, jz, vx, vy, vz = computeAnalyticMarginAndDerivatives(contacts, robot_name)
 plotAnalyticMarginAndDerivatives(mx, jx, my, jy, mz, jz, vx, vy, vz)
-learnedMargin.plot_learned_margin('com_jacobian_anymal_c_0111stance.txt')
+learnedMargin.plot_learned_margin('0111stance.txt')
 learnedMargin.set_plot_properties()
 
 fig3 = plt.figure(3)
@@ -119,7 +119,7 @@ fig3.suptitle("Analytic vs. Learned stability margin\n RF foot in stance (1011)"
 contacts = [1, 0, 1, 1]
 mx, jx, my, jy, mz, jz, vx, vy, vz = computeAnalyticMarginAndDerivatives(contacts, robot_name)
 plotAnalyticMarginAndDerivatives(mx, jx, my, jy, mz, jz, vx, vy, vz)
-learnedMargin.plot_learned_margin('com_jacobian_anymal_c_1011stance.txt')
+learnedMargin.plot_learned_margin('1011stance.txt')
 learnedMargin.set_plot_properties()
 
 fig4 = plt.figure(4)
@@ -127,7 +127,7 @@ fig4.suptitle("Analytic vs. Learned stability margin\n LH foot in stance (1101)"
 contacts = [1, 1, 0, 1]
 mx, jx, my, jy, mz, jz, vx, vy, vz = computeAnalyticMarginAndDerivatives(contacts, robot_name)
 plotAnalyticMarginAndDerivatives(mx, jx, my, jy, mz, jz, vx, vy, vz)
-learnedMargin.plot_learned_margin('com_jacobian_anymal_c_1101stance.txt')
+learnedMargin.plot_learned_margin('1101stance.txt')
 learnedMargin.set_plot_properties()
 
 fig5 = plt.figure(5)
@@ -135,7 +135,7 @@ fig5.suptitle("Analytic vs. Learned stability margin\n RH foot in stance (1110)"
 contacts = [1, 1, 1, 0]
 mx, jx, my, jy, mz, jz, vx, vy, vz = computeAnalyticMarginAndDerivatives(contacts, robot_name)
 plotAnalyticMarginAndDerivatives(mx, jx, my, jy, mz, jz, vx, vy, vz)
-learnedMargin.plot_learned_margin('com_jacobian_anymal_c_1110stance.txt')
+learnedMargin.plot_learned_margin('1110stance.txt')
 learnedMargin.set_plot_properties()
 
 fig6 = plt.figure(6)
@@ -143,7 +143,7 @@ fig6.suptitle("Analytic vs. Learned stability margin\n 2 stance feet (0110)")
 contacts = [0, 1, 1, 0]
 mx, jx, my, jy, mz, jz, vx, vy, vz = computeAnalyticMarginAndDerivatives(contacts, robot_name)
 plotAnalyticMarginAndDerivatives(mx, jx, my, jy, mz, jz, vx, vy, vz)
-learnedMargin.plot_learned_margin('com_jacobian_anymal_c_0110stance.txt')
+learnedMargin.plot_learned_margin('0110stance.txt')
 learnedMargin.set_plot_properties()
 
 fig7 = plt.figure(7)
@@ -151,7 +151,7 @@ fig7.suptitle("Analytic vs. Learned stability margin\n 2 stance feet (1001)")
 contacts = [0, 1, 1, 0]
 mx, jx, my, jy, mz, jz, vx, vy, vz = computeAnalyticMarginAndDerivatives(contacts, robot_name)
 plotAnalyticMarginAndDerivatives(mx, jx, my, jy, mz, jz, vx, vy, vz)
-learnedMargin.plot_learned_margin('com_jacobian_anymal_c_1001stance.txt')
+learnedMargin.plot_learned_margin('1001stance.txt')
 learnedMargin.set_plot_properties()
 
 plt.show()

jet_leg/computational_geometry/iterative_projection_parameters.py
@@ -466,7 +466,7 @@ class IterativeProjectionParameters:
         mu = 0.5
 
         ''' stanceFeet vector contains 1 is the foot is on the ground and 0 if it is in the air'''
-        stanceFeet = [1, 1, 0, 1]
+        stanceFeet = [1, 1, 1, 1]
 
         randomSwingLeg = random.randint(0, 3)
         tripleStance = False  # if you want you can define a swing leg using this variable
@@ -485,7 +485,7 @@ class IterativeProjectionParameters:
         normals = np.vstack([n1, n2, n3, n4])
 
         ''' Roll Pitch Yaw angles of the base link'''
-        rpy_base = np.array([0.0, 0.0, 0.0])  # units are rads
+        rpy_base = np.array([0., 0., 0.0])  # units are rads
         rot = Rot.from_euler('xyz', [rpy_base[0], rpy_base[1], rpy_base[2]], degrees=False)
         W_R_B = rot.as_dcm()
 
@@ -499,10 +499,21 @@ class IterativeProjectionParameters:
         LH_foot = model.nominal_stance_LH
         RH_foot = model.nominal_stance_RH
 
+        #LF_foot = [ -0.01,  0.19, -0.46] #model.nominal_stance_LF
+        #RF_foot = [ -0.01, -0.19, -0.46] #model.nominal_stance_RF
+        #LH_foot = [-0.59,  0.19, -0.46] #model.nominal_stance_LH
+        #RH_foot = [-0.59, -0.19, -0.46] #model.nominal_stance_RH
+
+        #LF_foot = [ 0.03597333,  0.14325718, - 0.47537701]
+        #RF_foot = [ 0.03597333, - 0.2348444, - 0.43744031]
+        #LH_foot = [-0.54112909,  0.13747649, - 0.53299111]
+        #RH_foot = [-0.54112909, - 0.2406251, - 0.49505442]
+
         contactsBF = np.vstack((LF_foot, RF_foot, LH_foot, RH_foot))
+        print contactsBF
         contactsWF = copy(contactsBF);
-        #for j in np.arange(0, 4):
-        #    contactsWF[j, :] = np.add(np.dot(W_R_B, copy(contactsBF[j, :])), comWF)
+        for j in np.arange(0, 4):
+            contactsWF[j, :] = np.add(np.dot(W_R_B, copy(contactsBF[j, :])), comWF)
         self.setContactsPosWF(contactsWF)
         self.setEulerAngles(rpy_base)
         self.useContactTorque = True

jet_leg/constraints/constraints.py
@@ -36,7 +36,7 @@ class Constraints:
         contactsNumber = np.sum(stanceLegs)
 
         contactsBF = copy.copy(params.computeContactsPosBF())
-        print "contacts BF ", contactsBF
+        #print "contacts BF ", contactsBF
         #print 'stance legs ', stanceLegs
         
         constraint_mode = params.getConstraintModes()
@@ -57,7 +57,7 @@ class Constraints:
 
         start_t_ik = time.time()
         self.kin.inverse_kin(contactsBF, foot_vel, stanceIndex)
-        print "IK time ", time.time() - start_t_ik
+        #print "IK time ", time.time() - start_t_ik
         #print ("q is ",q)
 
         forcePolytopes = LegForcePolytopes()

jet_leg/dynamics/rigid_body_dynamics.py
@@ -23,7 +23,7 @@ class RigidBodyDynamics:
         grav_vec = np.array([0.0, 0.0, robotsMass * gravity])
         static_linear = grav_vec + extForce
         static_angular = extTau
-        inertial_linear = np.multiply(robotsMass, comLinAcc)
+        inertial_linear = np.multiply(0.0, comLinAcc)
         #print "inertial_linear", inertial_linear
         #print "static_linear", static_linear
         #print "static_linear", static_linear
@@ -34,7 +34,7 @@ class RigidBodyDynamics:
         coriolis = coriolis[0]
         inertial_angular = np.matmul(robotInertia, comAngAcc) + coriolis
         ''' see eq. 3.9 at page 39 of Hongkai Dai's PhD thesis'''
-        linear_aggr_wrench = - inertial_linear - static_linear
+        linear_aggr_wrench = inertial_linear - static_linear
         #print "linear_aggr_wrench", linear_aggr_wrench
         angular_aggr_wrench = inertial_angular - static_angular
         total_wrench = np.hstack([linear_aggr_wrench, angular_aggr_wrench])

jet_leg/optimization/jacobians.py
@@ -62,21 +62,21 @@ class Jacobians:
 
         return jacobian
 
-    def computeComLinAccJacobian(self, params, contactsWF):
+    def computeComLinAccJacobian(self, params, ref_type):
         jacobian = np.zeros(3)
-        default = copy(contactsWF)
+        default_params = copy(params)
+        default_acc = copy(params.comLinAcc)
 
         for j in np.arange(0, 3):
-            feet_pos_dim = copy(default[:, j])
-            feet_pos_dim += self.delta
-            feet_pos_WF_plus = copy(default)
-            feet_pos_WF_plus[:, j] = feet_pos_dim
-            isPointFeasible, margin1 = self.computeMargin(params, feet_pos_WF_plus, "ZMP")
-            feet_pos_dim = copy(default[:, j])
-            feet_pos_dim -= self.delta
-            feet_pos_WF_minus = copy(default)
-            feet_pos_WF_minus[:, j] = feet_pos_dim
-            isPointFeasible, margin2 = self.computeMargin(params, feet_pos_WF_minus, "ZMP")
+            params_plus = copy(default_params)
+            params_plus.comLinAcc = default_acc
+            params_plus.comLinAcc[j] += self.delta
+            isPointFeasible, margin1 = self.computeMargin(params_plus, ref_type)
+
+            params_minus = copy(default_params)
+            params_plus.comLinAcc = default_acc
+            params_minus.comLinAcc[j] -= self.delta
+            isPointFeasible, margin2 = self.computeMargin(params_minus, ref_type)
             diff = margin1 - margin2
             jacobian[j] = diff / (2.0*self.delta)
 
@@ -136,23 +136,6 @@ class Jacobians:
 
         return jacobian
 
-    def computeComLinAccJacobian(self, params):
-        jacobian = np.zeros(3)
-        initiaPoint = params.comLinAcc
-
-        for j in np.arange(0, 3):
-            params.comLinAcc = initiaPoint
-            params.comLinAcc[j] = initiaPoint[j] + self.delta / 2.0
-            isPointFeasible, margin1 = self.compDyn.compute_IP_margin(params, "COM")
-
-            params.comLinAcc = initiaPoint
-            params.comLinAcc[j] = initiaPoint[j] - self.delta / 2.0
-            isPointFeasible, margin2 = self.compDyn.compute_IP_margin(params, "COM")
-            diff = margin1 - margin2
-            jacobian[j] = diff / self.delta
-
-        return jacobian
-
     def computeFeetJacobian(self, params):
         jacobian = np.zeros(12)
         for footID in np.arange(0, 4):
@@ -167,79 +150,40 @@ class Jacobians:
             contacts = initialContacts
             contacts[footID, j] = initiaPoint[j] + self.delta / 2.0
             params.setContactsPosWF(contacts)
-            isPointFeasible, margin1 = self.compDyn.compute_IP_margin(params, "COM")
+            isPointFeasible, margin1 = self.compDyn.compute_IP_margin(params, "ZMP")
             contacts = initialContacts
             contacts[footID, j] = initiaPoint[j] - self.delta / 2.0
             params.setContactsPosWF(contacts)
-            isPointFeasible, margin2 = self.compDyn.compute_IP_margin(params, "COM")
+            isPointFeasible, margin2 = self.compDyn.compute_IP_margin(params, "ZMP")
             diff = margin1 - margin2
             jacobian[j] = diff / self.delta
 
         return jacobian
 
-    def plotMarginAndJacobianOfMarginWrtComLinAcceleration(self, params, acc_range, dimension):
-
-        #num_of_tests = np.shape(com_pos_range)
-        #margin = np.zeros(num_of_tests)
-        #jac_com_lin_acc = np.zeros((3, num_of_tests[0]))
-        #count = 0
-        #default_feet_pos_WF = copy(params.getContactsPosWF())
-        #
-        #for delta in com_pos_range:
-        #    """ contact points in the World Frame"""
-        #    contactsWF = copy(default_feet_pos_WF)
-        #    contactsWF[:, dim_to_check] += delta
-        #    isPointFeasible, margin[count] = self.computeComMargin(params, contactsWF)
-        #    jac_com_lin_acc[:, count] = self.computeComPosJacobian(params, contactsWF)
-        #
-        #    count += 1
-        #
-        #params.setContactsPosWF(default_feet_pos_WF)Com
-        #return margin, jac_com_lin_acc
-
+    def plotMarginAndJacobianWrtComLinAcceleration(self, params, acc_range, dimension):
 
         num_of_tests = np.shape(acc_range)
         margin = np.zeros(num_of_tests)
-        jac_com_lin_acc = np.zeros((3, num_of_tests[0]))
+        jac_com_pos = np.zeros((3, num_of_tests[0]))
         count = 0
-        for delta_acc in acc_range:
-            #print "dimension", dimension
-            #print "delta_acc", delta_acc
-            params.comLinAcc = [0.0, 0.0, 0.0]
-            params.comLinAcc[dimension] = delta_acc
-            #print "count", count, params.comLinAcc, params.comPositionWF, params.getContactsPosWF()
-            ''' compute iterative projection 
-            Outputs of "iterative_projection_bretl" are:
-            IP_points = resulting 2D vertices
-            actuation_polygons = these are the vertices of the 3D force polytopes (one per leg)
-            computation_time = how long it took to compute the iterative projection
-            '''
-            IP_points, force_polytopes, IP_computation_time = self.compDyn.try_iterative_projection_bretl(params)
+        default_params = copy(params)
+        default_acc = deepcopy(params.comLinAcc)
+        reference_type = "ZMP"
 
-            '''I now check whether the given CoM configuration is stable or not'''
-            isCoMStable, contactForces, forcePolytopes = self.compDyn.check_equilibrium(params)
-            #print "is CoM stable?", isCoMStable
-            # print 'Contact forces:', contactForces
-            #print 'IP_points:', IP_points
-            #if IP_points is True:
-            #    print "ip points shape", np.shape(IP_points)
-            #    facets = self.compGeom.compute_halfspaces_convex_hull(IP_points)
-            #    point2check = self.compDyn.getReferencePoint(params)
-            #    print "point 2 check", point2check
-            #    isPointFeasible, margin[count] = self.compGeom.isPointRedundant(facets, point2check)
-            isPointFeasible, margin[count] = self.compDyn.compute_IP_margin(params, "COM")
-            #
-            marginJAcWrtComLinAcc = self.computeComLinAccJacobian(params)
-            #    print "marginJAcWrtComLinAcc", marginJAcWrtComLinAcc
-            jac_com_lin_acc[:, count] = marginJAcWrtComLinAcc
-            #else:
-            #    jac_com_lin_acc[:, count] = [-1000] * 3
-
-            # print "com lin vel jacobian", jac_com_lin_vel
+        for delta in acc_range:
+            """ contact points in the World Frame"""
+            tmp_params = copy(default_params)
+            params.comLinAcc = default_acc
+            tmp_params.comLinAcc = default_acc
+            tmp_params.comLinAcc[dimension] = delta
+            isPointFeasible, margin[count] = self.computeMargin(tmp_params, reference_type)
+            jac_com_pos[:, count] = self.computeComLinAccJacobian(tmp_params, reference_type)
 
             count += 1
 
-        return margin, jac_com_lin_acc
+        print "params.comLinAcc", params.comLinAcc
+
+        return margin, jac_com_pos
 
     def plotMarginAndJacobianOfMarginWrtComVelocity(self, params, velocity_range, dimension):
         num_of_tests = np.shape(velocity_range)
@@ -308,7 +252,6 @@ class Jacobians:
 
             count += 1
 
-        params.setContactsPosWF(default_feet_pos_WF)
         return margin, jac_com_pos
 
     '''